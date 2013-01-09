4072e7f4d16ab71a845f93e017cc13e960964c20
Pierre du Plessis <pierre@pcservice.co.za>
2013-01-09 09:08:26 +0200

    [Core] Mark tests as incomplete

--- src/CSBill/CoreBundle/Tests/Controller/DashboardControllerTest.php
+++ src/CSBill/CoreBundle/Tests/Controller/DashboardControllerTest.php
@@ -17,10 +17,8 @@ class DefaultControllerTest extends WebTestCase
 {
     public function testIndex()
     {
-        $client = static::createClient();
-
-        $crawler = $client->request('GET', '/dashboard');
-
-        $this->assertTrue($crawler->filter('html:contains("")')->count() > 0);
+    	$this->markTestIncomplete(
+    			'This test has not been implemented yet.'
+    	);
     }
 }
