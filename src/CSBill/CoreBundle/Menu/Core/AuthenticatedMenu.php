d000bacaba98a280c15a63dc734b52acb5d80891
Pierre du Plessis <pierre@pcservice.co.za>
2013-01-11 16:32:13 +0200

    [Core] Added core Authenticated Menu, to check if users are authenticated before displaying the menu

---
+++ src/CSBill/CoreBundle/Menu/Core/AuthenticatedMenu.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace CSBill\CoreBundle\Menu\Core;
+
+use CSBill\CoreBundle\Menu\Builder\BuilderInterface;
+use Symfony\Component\DependencyInjection\ContainerAware;
+use SYmfony\Component\Security\Core\Exception\AuthenticationCredentialsNotFoundException;
+
+class AuthenticatedMenu extends ContainerAware implements BuilderInterface
+{
+	public function validate()
+	{
+		try {
+			$security = $this->container->get('security.context');
+		} catch(AuthenticationCredentialsNotFoundException $e) {
+			return false;
+		}
+
+		return $security->isGranted('IS_AUTHENTICATED_FULLY');
+	}
+}
