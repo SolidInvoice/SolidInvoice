5731c53cb72a070c97fd9c2a5b0b8744a330cf24
Pierre du Plessis <pierre@pcservice.co.za>
2013-02-23 12:22:38 +0200

    [Core] Added local file overrides for menu

---
+++ src/CSBill/CoreBundle/Menu/Factory.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * This file is part of the CSBill package.
+ *
+ * (c) Pierre du Plessis <info@customscripts.co.za>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CSBill\CoreBundle\Menu;
+
+use Knp\Menu\Silex\RouterAwareFactory;
+
+class Factory extends RouterAwareFactory
+{
+}
