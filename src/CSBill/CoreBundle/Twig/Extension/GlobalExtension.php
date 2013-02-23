3f1856ccbf1b130c148b1911b21a975ed63a475d
Pierre du Plessis <pierre@pcservice.co.za>
2013-02-23 12:22:55 +0200

    [Core] Add global twig variables

---
+++ src/CSBill/CoreBundle/Twig/Extension/GlobalExtension.php
@@ -0,0 +1,79 @@
+<?php
+/*
+ * This file is part of the CSBill package.
+ *
+ * (c) Pierre du Plessis <info@customscripts.co.za>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CSBill\CoreBundle\Twig\Extension;
+
+use Twig_Extension;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\DependencyInjection\Exception\InactiveScopeException;
+
+class GlobalExtension extends Twig_Extension
+{
+	/**
+	 * @var ContainerInterface $container
+	 */
+    protected $container;
+
+    /**
+     * Sets the container
+     *
+     * @param ContainerInterface $container
+     */
+    public function setContainer(ContainerInterface $container)
+    {
+    	$this->container = $container;
+    }
+
+    /**
+     * Get global twig variables
+     *
+     * @return array
+     */
+    public function getGlobals()
+    {
+        return array(
+                    'sessionId' 		=> session_id(),
+                    'query'				=> $this->getQuery(),
+            );
+    }
+
+    /**
+     * Get the url query
+     *
+     * @throws InactiveScopeException
+     * @return array
+     */
+    protected function getQuery()
+    {
+        try {
+            $request = $this->container->get('request');
+
+            $params = array_merge($request->query->all(), $request->attributes->all());
+
+            foreach ($params as $key => $param) {
+                if (substr($key, 0, 1) == '_') {
+                    unset($params[$key]);
+                }
+            }
+
+            return $params;
+        } catch (InactiveScopeException $e) {
+            return array();
+        }
+    }
+
+    /**
+     * {inhertitDoc}
+     */
+	public function getName()
+	{
+		return 'csbill_core.twig.globals';
+	}
+ }
