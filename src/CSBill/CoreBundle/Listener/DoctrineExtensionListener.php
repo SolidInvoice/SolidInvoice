68b3705851a5923f0eae8798cd2eb5fae8aa747b
Pierre du Plessis <pierre@pcservice.co.za>
2013-03-31 14:06:26 +0200

    [CoreBundle] Added listener class for doctrine extensions

---
+++ src/CSBill/CoreBundle/Listener/DoctrineExtensionListener.php
@@ -0,0 +1,55 @@
+<?php
+/*
+ * This file is part of the CSBill package.
+ *
+ * (c) Pierre du Plessis <info@customscripts.co.za>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CSBill\CoreBundle\Listener;
+
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class DoctrineExtensionListener implements ContainerAwareInterface
+{
+    /**
+     * @var ContainerInterface
+     */
+    protected $container;
+
+    /**
+     * {@inheritDoc}
+     *
+     * @param ContainerInterface $container
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * @param GetResponseEvent $event
+     */
+    public function onLateKernelRequest(GetResponseEvent $event)
+    {
+        $translatable = $this->container->get('gedmo.listener.translatable');
+        $translatable->setTranslatableLocale($event->getRequest()->getLocale());
+    }
+
+    /**
+     * @param GetResponseEvent $event
+     */
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        $securityContext = $this->container->get('security.context', ContainerInterface::NULL_ON_INVALID_REFERENCE);
+
+        if (null !== $securityContext && null !== $securityContext->getToken() && $securityContext->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+            $loggable = $this->container->get('gedmo.listener.loggable');
+            $loggable->setUsername($securityContext->getToken()->getUsername());
+        }
+    }
+}
