d6b92e46bf49e982b921b3e7da8236be49b309b7
Pierre du Plessis <pierre@pcservice.co.za>
2013-03-31 14:03:42 +0200

    [ClientBundle] Added ajax controller

---
+++ src/CSBill/ClientBundle/Controller/AjaxController.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * This file is part of the CSBill package.
+ *
+ * (c) Pierre du Plessis <info@customscripts.co.za>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CSBill\ClientBundle\Controller;
+
+use CS\CoreBundle\Controller\Controller;
+use CSBill\ClientBundle\Entity\Client;
+
+class AjaxController extends Controller
+{
+    /**
+     * Get client info
+     *
+     * @return Response
+     */
+    public function infoAction(Client $client)
+    {
+        return $this->render('CSBillClientBundle:Ajax:info.html.twig', array('client' => $client));
+    }
+}
