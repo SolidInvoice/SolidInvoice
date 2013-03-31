b093e36c0339d29ad521a3d1a72e931daaa0d6f4
Pierre du Plessis <pierre@pcservice.co.za>
2013-03-31 14:05:00 +0200

    [ClientBundle] Add Doctrine fixtures

---
+++ src/CSBill/ClientBundle/DataFixtures/ORM/LoadStatus.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of the CSBill package.
+ *
+ * (c) Pierre du Plessis <info@customscripts.co.za>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CSBill\ClientBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use CSBill\ClientBundle\Entity\Status;
+
+class LoadStatus implements FixtureInterface
+{
+	/*
+	 * (non-phpdoc)
+	 */
+    public function load(ObjectManager $manager)
+    {
+        // Active
+        $active = new Status();
+        $active->setName('active');
+        $manager->persist($active);
+
+        // InActive
+        $inActive = new Status();
+        $inActive->setName('inactive');
+        $manager->persist($inActive);
+
+        // flush client statuses
+        $manager->flush();
+    }
+}
