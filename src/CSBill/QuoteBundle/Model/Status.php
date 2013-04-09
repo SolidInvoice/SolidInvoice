6587d646164ca30870004b0fbd590804e3902c63
Pierre du Plessis <pierre@pcservice.co.za>
2013-04-09 15:30:59 +0200

    [QuoteBundle] Added Quote status model

---
+++ src/CSBill/QuoteBundle/Model/Status.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of the CSBill package.
+ *
+ * (c) Pierre du Plessis <info@customscripts.co.za>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CSBill\QuoteBundle\Model;
+
+use CSBill\CoreBundle\Model\Status as AbstractStatus;
+
+class Status extends AbstractStatus
+{
+    /**
+     * Contains a list of all the statuses and their corresponding labels
+     *
+     * @var array
+     */
+    protected $statusLabels = array(
+                                    'paid'     => 'success',
+                                    'pending'     => 'warning'
+            );
+}
