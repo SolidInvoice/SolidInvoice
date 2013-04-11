f88e7ab34cfea4bed2729d97719d048bb15e34d5
Pierre du Plessis <pierre@pcservice.co.za>
2013-04-11 13:33:26 +0200

    [QuoteBundle] Added DataFixtures

---
+++ src/CSBill/QuoteBundle/DataFixtures/ORM/LoadStatus.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of the CSUserBundle package.
+ *
+ * (c) Pierre du Plessis <info@customscripts.co.za>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CSBull\QuoteBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use CSBill\QuoteBundle\Entity\Status;
+
+class LoadStatus implements FixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $statusList = array(
+                            'pending',
+                            'accepted',
+                            'declined',
+                            'draft',
+                            'cancelled',
+                            );
+
+        foreach($statusList as $status) {
+            $entity = new Status;
+            $entity->setName($status);
+            $manager->persist($entity);
+        }
+
+        $manager->flush();
+    }
+}
