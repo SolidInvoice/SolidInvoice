a36bf4442472d0ea8ce74a7c5e2aec262dd819a0
Pierre du Plessis <pierre@pcservice.co.za>
2013-04-12 13:05:24 +0200

    [CoreBundle] Add right-side menu with logout

---
+++ src/CSBill/CoreBundle/Menu/Main.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * This file is part of the CSBill package.
+ *
+ * (c) Pierre du Plessis <info@customscripts.co.za>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CSBill\CoreBundle\Menu;
+
+use Symfony\Component\Security\Core\SecurityContext;
+
+use Knp\Menu\ItemInterface;
+use CSBill\CoreBundle\Menu\Core\AuthenticatedMenu;
+
+class Main extends AuthenticatedMenu
+{
+    /**
+     * Menu builder for the quotes index
+     *
+     * @param $menu \Knp\Menu\ItemInterface
+     */
+    public function topMenu(ItemInterface $menu, array $parameters = array())
+    {
+        $user = $this->container->get('security.context')->getToken()->getUser();
+
+        $username = $user->getUsername() . ' <b class="caret"></b>';
+
+        $menu->addChild($username, array('uri' => '#', 'allow_safe_labels' => true, 'extras' => array('safe_label' => true, 'icon' => 'icon-user')));
+
+        $topMenu = $menu[$username];
+
+        $topMenu->setAttributes(array('class' => 'dropdown'));
+        $topMenu->setLinkAttributes(array('class' => 'dropdown-toggle', 'data-toggle' => 'dropdown'));
+
+        $topMenu->addDivider();
+        $topMenu->addChild('Logout', array('uri' => '_logout', 'extras' => array('icon' => 'icon-off')));
+
+        $topMenu->setChildrenAttributes(array('class' => 'dropdown-menu'));
+    }
+}
