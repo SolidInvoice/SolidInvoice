b115aa9a78e8a42398399afa2a6ef42df41551d9
Pierre du Plessis <pierre@pcservice.co.za>
2013-05-10 12:17:11 +0200

    [InvoiceBundle] Updated grid - removed un-used code and optimized grid

--- src/CSBill/InvoiceBundle/Controller/DefaultController.php
+++ src/CSBill/InvoiceBundle/Controller/DefaultController.php
@@ -18,7 +18,6 @@ use APY\DataGridBundle\Grid\Column\ActionsColumn;
 use APY\DataGridBundle\Grid\Action\RowAction;
 use APY\DataGridBundle\Grid\Row;
 use APY\DataGridBundle\Grid\Action\DeleteMassAction;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -31,70 +30,46 @@ class DefaultController extends Controller
         // Get a Grid instance
         $grid = $this->get('grid');
 
-        /*$request = $this->getRequest();
-
-        // TODO : get better way of adding filters & search instead of defining it in the controller like this
-        $filters = new Filters($this->getRequest());
-
-        $filters->add('all_clients', null, true, array('active_class' => 'label label-info', 'default_class' => 'label'));
-
-        // TODO : get status from database
-        $statuses = new Status;
-        foreach ($statuses->getStatusList() as $status) {
-            $filters->add($status.'_clients', function(QB $qb) use ($status) {
-                $alias = $qb->getRootAlias();
-
-                $qb->join($alias.'.status', 's')
-                ->andWhere('s.name = :status_name')
-                ->setParameter('status_name', $status);
-            }, false, array('active_class' => 'label label-' . $statuses->getStatusLabel($status), 'default_class' => 'label'));
-        }
-
-        $search = $this->getRequest()->get('search');
-
-        $source->manipulateQuery(function(QB $qb) use ($search, $filters) {
-
-            if ($filters->isFilterActive()) {
-                $filter = $filters->getActiveFilter();
-                $filter($qb);
-            }
-
-            if ($search) {
-                $alias = $qb->getRootAlias();
-
-                $qb->andWhere($alias.'.name LIKE :search')
-                ->setParameter('search', "%{$search}%");
-            }
-        });*/
-
-
         // Attach the source to the grid
         $grid->setSource($source);
 
-
         // Custom actions column in the wanted position
-        $viewColumn = new ActionsColumn('info_column', $this->get('translator')->trans('Info'));
-        $grid->addColumn($viewColumn, 100);
+        //$viewColumn = new ActionsColumn('info_column', $this->get('translator')->trans('Info'));
+        //$grid->addColumn($viewColumn, 100);
 
-        $viewAction = new RowAction($this->get('translator')->trans('View'), '_invoices_view');
-        $viewAction->setColumn('info_column');
-        $grid->addRowAction($viewAction);
+        $viewAction = new RowAction($this->trans('View'), '_invoices_view');
+        $viewAction->setAttributes(array('class' => 'btn'));
 
-        $editColumn = new ActionsColumn('edit_column', $this->get('translator')->trans('Edit'));
-        $grid->addColumn($editColumn, 200);
+        //$viewAction->setColumn('info_column');
+        //$grid->addRowAction($viewAction);
 
-        $editAction = new RowAction($this->get('translator')->trans('Edit'), '_invoices_edit');
-        $editAction->setColumn('edit_column');
-        $grid->addRowAction($editAction);
+        //$editColumn = new ActionsColumn('edit_column', $this->get('translator')->trans('Edit'));
+        //$grid->addColumn($editColumn, 200);
 
-        $grid->addMassAction(new DeleteMassAction());
+        $editAction = new RowAction($this->trans('Edit'), '_invoices_edit');
+        $editAction->setAttributes(array('class' => 'btn'));
+        //$editAction->setColumn('edit_column');
+        //$grid->addRowAction($editAction);
+
+
+
+        $actionsRow = new ActionsColumn('actions', 'Action', array($editAction, $viewAction));
+        $grid->addColumn($actionsRow, 100);
+
+        //$grid->addMassAction(new DeleteMassAction());
+
+        $grid->hideColumns(array('updated', 'deleted', 'users', 'paidDate', 'due', 'baseTotal'));
+
         $statusList = $this->getRepository('CSBillInvoiceBundle:Status')->findAll();
 
-        $grid->hideColumns(array('updated', 'deleted'));
 
 
         // Return the response of the grid to the template
-        return $grid->getGridResponse('CSBillInvoiceBundle:Default:index.html.twig', array('status_list' => $statusList));
+        return $grid->getGridResponse('CSBillInvoiceBundle:Default:index.html.twig',
+                    array(
+                            'status_list'       => $statusList,
+                        )
+                );
     }
 
     public function createAction()
