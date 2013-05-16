52b86044a4260b8fa4ff8c68cb1e1cb9b64a98da
Pierre du Plessis <pierre@pcservice.co.za>
2013-05-16 14:45:34 +0200

    [CoreBundle] Added comments for js file

--- src/CSBill/CoreBundle/Resources/public/js/script.js
+++ src/CSBill/CoreBundle/Resources/public/js/script.js
@@ -1,9 +1,18 @@
 $(function(){
+    /**
+     * Tooltip
+     */
     $('body').tooltip({
         "selector" : '[rel=tooltip]'
     });
 
+    /**
+     * Chosen
+     */
     $('select.chosen').chosen();
 
+    /**
+     * PlaceHolder
+     */
     $('input[placeholder]').placeholder();
 });
