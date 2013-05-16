32a24df0e10d6daaf8c811c667465de7d903b09a
Pierre du Plessis <pierre@pcservice.co.za>
2013-05-16 14:45:00 +0200

    [QuoteBundle] Removed un-used code in controller

--- src/CSBill/QuoteBundle/Controller/DefaultController.php
+++ src/CSBill/QuoteBundle/Controller/DefaultController.php
@@ -16,8 +16,6 @@ use CSBill\QuoteBundle\Entity\Quote;
 use APY\DataGridBundle\Grid\Source\Entity;
 use APY\DataGridBundle\Grid\Column\ActionsColumn;
 use APY\DataGridBundle\Grid\Action\RowAction;
-use APY\DataGridBundle\Grid\Row;
-use APY\DataGridBundle\Grid\Action\DeleteMassAction;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -33,27 +31,14 @@ class DefaultController extends Controller
         // Attach the source to the grid
         $grid->setSource($source);
 
-        // Custom actions column in the wanted position
-        //$viewColumn = new ActionsColumn('info_column', $this->get('translator')->trans('Info'));
-        //$grid->addColumn($viewColumn, 100);
-
         $viewAction = new RowAction($this->get('translator')->trans('View'), '_quotes_view');
         $viewAction->setAttributes(array('class' => 'btn'));
-        //$viewAction->setColumn('info_column');
-        //$grid->addRowAction($viewAction);
-
-        //$editColumn = new ActionsColumn('edit_column', $this->get('translator')->trans('Edit'));
-        //$grid->addColumn($editColumn, 200);
 
         $editAction = new RowAction($this->get('translator')->trans('Edit'), '_quotes_edit');
         $editAction->setAttributes(array('class' => 'btn'));
-        //$editAction->setColumn('edit_column');
-        //$grid->addRowAction($editAction);
 
         $actionsRow = new ActionsColumn('actions', 'Action', array($editAction, $viewAction));
         $grid->addColumn($actionsRow, 100);
-
-        //$grid->addMassAction(new DeleteMassAction());
 
         $grid->hideColumns(array('updated', 'deleted', 'users', 'due', 'baseTotal'));
 
