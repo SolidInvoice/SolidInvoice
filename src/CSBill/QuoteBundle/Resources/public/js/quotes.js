0fe587b4017cd2ccb9c272af5207fa8488770528
Pierre du Plessis <pierre@pcservice.co.za>
2013-05-21 12:24:03 +0200

    [QuoteBundle] Updated quote.js

--- src/CSBill/QuoteBundle/Resources/public/js/quotes.js
+++ src/CSBill/QuoteBundle/Resources/public/js/quotes.js
@@ -1,132 +1,134 @@
-!function($, _, window){
+(function($, window) {
 
-    "use strict"; // jshint ;_;
+    "use strict";
 
     var Quote = {
-                    "el" : null,
-                    "rowElement" : "tr",
-                    "columnElement" : "td",
-                    "fields" : [],
-                    "templates" : {},
-                    "trashTemplate" : '<div class="pull-right"><a href="#" class="remove-item" rel="tooltip" title="Remove Item"><i class="icon-trash"></i></a></div>',
-                    "counter" : 0,
-                    "addTemplate" : function(type, template) {
-                        this.templates[type] = template;
-                        return this;
-                    },
-                    "addField" : function(field) {
-                        this.fields.push(field);
-                        return this;
-                    },
-                    "setSelector" : function(selector) {
-                        this.el = $('tbody', selector);
-                        return this;
-                    },
-                    "addRow" : function() {
-                        var that         = this,
-                            row         = $(window.document.createElement(this.rowElement)),
-                            totalFields = this.fields.length;
+            "el" : null,
+            "rowElement" : "tr",
+            "columnElement" : "td",
+            "fields" : [],
+            "templates" : {},
+            "trashTemplate" : '<div class="pull-right"><a href="#" class="remove-item" rel="tooltip" title="Remove Item"><i class="icon-trash"></i></a></div>',
+            "counter" : 0,
+            "addTemplate" : function(type, template) {
+                this.templates[type] = template;
+                return this;
+            },
+            "addField" : function(field) {
+                this.fields.push(field);
+                return this;
+            },
+            "setSelector" : function(selector) {
+                this.el = $('tbody', selector);
+                return this;
+            },
+            "addRow" : function() {
+                var that         = this,
+                    row         = $(window.document.createElement(this.rowElement)),
+                    //totalFields = this.fields.length,
+                    rowTotal = $('.quote-item-total', row);
 
 
-                        _.each(this.fields, function(item, counter, fields) {
-                            var column = $(window.document.createElement(that.columnElement));
+                $.each(this.fields, function(counter, item) {
+                    var column = $(window.document.createElement(that.columnElement));
 
-                            column.html(that.createField(item));
+                    column.html(that.createField(item));
 
-                            row.append(column);
+                    row.append(column);
 
-                            if((counter + 1) === totalFields) {
-                                //column.append(_.template(that.trashTemplate));
-                            }
-                        });
+                    /*if ((counter + 1) === totalFields) {
+                        //column.append(_.template(that.trashTemplate));
+                    }*/
+                });
 
-                        this.counter++;
+                this.counter += 1;
 
-                        row.hide();
+                row.hide();
 
-                        this.el.append(row);
+                this.el.append(row);
 
-                        row.fadeIn(150, this.setEvents);
+                row.fadeIn(150, this.setEvents);
 
-                        var rowTotal = $('.quote-item-total', row);
+                rowTotal.val(accounting.formatMoney(rowTotal.val() || 0, ''));
 
-                        rowTotal.val(accounting.formatMoney(rowTotal.val() || 0, ''));
+                return this;
+            },
+            "createField" : function(item) {
+                //var length = this.el.find(this.rowElement).length;
+                var template = this.templates[item].replace(/__name__/g, this.counter);
 
-                        return this;
-                    },
-                    "createField" : function(item) {
-                        //var length = this.el.find(this.rowElement).length;
-                        var template = this.templates[item].replace(/__name__/g, this.counter);
+                return template;
+            },
+            "setEvents" : function() {
+                $('.quote-item-price, .quote-item-qty').unbind('keyup change');
 
-                        return template;
-                    },
-                    "setEvents" : function() {
-                        $('.quote-item-price, .quote-item-qty').unbind('keyup change');
+                $('.quote-item-price, .quote-item-qty').on('keyup', function() {
+                    Quote.calcTotal(this);
+                });
 
-                         $('.quote-item-price, .quote-item-qty').on('keyup', function(evt){
-                             Quote.calcTotal(this);
-                         });
+                $('.quote-item-qty').on('change', function() {
+                    var qty = $(this),
+                        val = qty.val(),
+                        decimals,
+                        value;
 
-                         $('.quote-item-qty').on('change', function(evt){
-                             var qty = $(this),
-                                 val = qty.val();
-
-                             if(val.indexOf('.') !== -1) {
-                                 var decimals = val.substr(val.indexOf('.') + 1);
-                                 qty.val(accounting.toFixed(qty.val(), decimals.length > 2 ? 2 : decimals.length) || 1);
-                             } else {
-                                 var value = accounting.toFixed(qty.val());
-                                 qty.val(value > 0 ? value : 1);
-                             }
-                             Quote.calcTotal(this);
-                         });
-
-                         $('.quote-item-price').on('change', function(evt){
-                             var price = $(this);
-                             price.val(accounting.formatMoney(price.val(), '', '2'));
-
-                             Quote.calcTotal(this);
-                         });
-                    },
-                    "calcTotal" : function(row) {
-                        var tr = $(row).parents('tr');
-
-                        var price = accounting.unformat($('.quote-item-price', tr).val()),
-                            qty   = $('.quote-item-qty', tr).val(),
-                            total = $('.quote-item-total', tr);
-
-                        total.val(accounting.formatMoney(qty * price, ''));
-
-                        Quote.updateTotal();
-                    },
-                    "updateTotal" : function() {
-
-                        var subTotal = 0;
-                        $('.quote-item-total', this.el).each(function() {
-                            subTotal += parseFloat(accounting.unformat($(this).val()));
-                        });
-
-                        var discount = (subTotal * parseInt($('#quote_discount').val() || 0) / 100);
-
-                        $('.quote-discount').html(accounting.formatMoney(discount * -1));
-
-                        $('.quote-sub-total').html(accounting.formatMoney(subTotal));
-
-                        $('.quote-total').html(accounting.formatMoney(subTotal - discount));
+                    if (val.indexOf('.') !== -1) {
+                        decimals = val.substr(val.indexOf('.') + 1);
+                        qty.val(accounting.toFixed(qty.val(), decimals.length > 2 ? 2 : decimals.length) || 1);
+                    } else {
+                        value = accounting.toFixed(qty.val());
+                        qty.val(value > 0 ? value : 1);
                     }
-                };
+                    Quote.calcTotal(this);
+                });
 
-    $(function(){
-        $('.add-item').on('click', function(evt){
+                $('.quote-item-price').on('change', function() {
+                    var price = $(this);
+                    price.val(accounting.formatMoney(price.val(), '', '2'));
+
+                    Quote.calcTotal(this);
+                });
+            },
+            "calcTotal" : function(row) {
+                var tr = $(row).parents('tr'),
+                    price = accounting.unformat($('.quote-item-price', tr).val()),
+                    qty   = $('.quote-item-qty', tr).val(),
+                    total = $('.quote-item-total', tr);
+
+                total.val(accounting.formatMoney(qty * price, ''));
+
+                Quote.updateTotal();
+            },
+            "updateTotal" : function() {
+
+                var subTotal = 0,
+                    discount;
+
+                $('.quote-item-total', this.el).each(function() {
+                    subTotal += parseFloat(accounting.unformat($(this).val()));
+                });
+
+                discount = (subTotal * parseInt($('#quote_discount').val() || 0, 10) / 100);
+
+                $('.quote-discount').html(accounting.formatMoney(discount * -1));
+
+                $('.quote-sub-total').html(accounting.formatMoney(subTotal));
+
+                $('.quote-total').html(accounting.formatMoney(subTotal - discount));
+            }
+        };
+
+    $(function() {
+        $('.add-item').on('click', function(evt) {
             evt.preventDefault();
 
             Quote.addRow();
         });
 
-        $('.quote-item-price, .quote-item-total', Quote.el).each(function(){
+        $('.quote-item-price, .quote-item-total', Quote.el).each(function() {
             var $this = $(this);
 
-            if($this.val() !== '') {
+            if ($this.val() !== '') {
                 $this.val(accounting.formatMoney($this.val(), ''));
             }
 
@@ -135,8 +137,8 @@
 
         $('#quote_discount').on('change keyup', Quote.updateTotal);
 
-        $('#quote-create-form').on('submit', function(){
-            $('.quote-item-price', this).each(function(){
+        $('#quote-create-form').on('submit', function() {
+            $('.quote-item-price', this).each(function() {
                 var $this = $(this);
                 $this.val(accounting.unformat($this.val()));
             });
@@ -146,11 +148,11 @@
 
         $(Quote.el.selector).on('click', '.remove-item', function(evt) {
             evt.preventDefault();
-            $(this).parents('tr').fadeOut(function(){
+            $(this).parents('tr').fadeOut(function() {
                 $(this).remove();
                 Quote.updateTotal();
 
-                if($(Quote.rowElement, Quote.el).length === 0) {
+                if ($(Quote.rowElement, Quote.el).length === 0) {
                     Quote.addRow();
                 }
             });
@@ -160,4 +162,4 @@
     });
 
     window.Quote = Quote;
-}(jQuery, _, window);
+})(jQuery, window);
