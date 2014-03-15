04c54f2ec07d27333b9e7127e15449af85b2df9a
Pierre du Plessis <pierre@pcservice.co.za>
2014-03-15 19:21:29 +0200

    Added database migrations and updated security

--- app/DoctrineMigrations/Version20140315120936.php
+++ app/DoctrineMigrations/Version20140315120936.php
@@ -21,6 +21,7 @@ class Version20140315120936 extends AbstractMigration
         $this->addSql("ALTER TABLE users ADD username_canonical VARCHAR(255) NOT NULL, ADD email_canonical VARCHAR(255) NOT NULL, ADD last_login DATETIME DEFAULT NULL, ADD locked TINYINT(1) NOT NULL, ADD expired TINYINT(1) NOT NULL, ADD expires_at DATETIME DEFAULT NULL, ADD confirmation_token VARCHAR(255) DEFAULT NULL, ADD password_requested_at DATETIME DEFAULT NULL, ADD roles LONGTEXT NOT NULL COMMENT '(DC2Type:array)', ADD credentials_expired TINYINT(1) NOT NULL, ADD credentials_expire_at DATETIME DEFAULT NULL, CHANGE username username VARCHAR(255) NOT NULL, CHANGE salt salt VARCHAR(255) NOT NULL, CHANGE email email VARCHAR(255) NOT NULL, CHANGE active enabled TINYINT(1) NOT NULL");
         $this->addSql("CREATE UNIQUE INDEX UNIQ_1483A5E992FC23A8 ON users (username_canonical)");
         $this->addSql("CREATE UNIQUE INDEX UNIQ_1483A5E9A0D96FBF ON users (email_canonical)");
+        $this->addSql("UPDATE users SET roles = 'a:1:{i:0;s:10:\"ROLE_ADMIN\";}', username_canonical = username, email_canonical = email, enabled = 1");
     }
 
     public function down(Schema $schema)
