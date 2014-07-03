b749ef04bcef056151d97903efbc1212a1f9ac6b
Pierre du Plessis <pierre@pcservice.co.za>
2014-07-03 22:21:49 +0200

    [PaymentBundle] Mark offline payments as success by default

---
+++ src/CSBill/PaymentBundle/Action/Offline/StatusAction.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace CSBill\PaymentBundle\Action\Offline;
+
+use Payum\Core\Action\ActionInterface;
+use Payum\Core\Bridge\Spl\ArrayObject;
+use Payum\Core\Exception\RequestNotSupportedException;
+use Payum\Core\Request\CaptureRequest;
+use Payum\Offline\Constants;
+
+class StatusAction implements ActionInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function execute($request)
+    {
+        /** @var $request \Payum\Core\Request\StatusRequestInterface */
+        if (false == $this->supports($request)) {
+            throw RequestNotSupportedException::createActionNotSupported($this, $request);
+        }
+
+        $model = ArrayObject::ensureArrayObject($request->getModel());
+
+        $model[Constants::FIELD_STATUS] = Constants::STATUS_SUCCESS;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function supports($request)
+    {
+        return
+            $request instanceof CaptureRequest &&
+            $request->getModel() instanceof \ArrayAccess
+            ;
+    }
+}
