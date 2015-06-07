ded1b238dcaeb4a1966148c8c8ed8e2b75e445c5
Pierre du Plessis <pierre@pcservice.co.za>
2015-06-07 14:31:04 +0200

    Append fixtures

--- travis/phpunit.sh
+++ travis/phpunit.sh
@@ -1,5 +1,5 @@
 mysql -e "drop database csbill"
 php app/console doctrine:database:create -n
 php app/console doctrine:migrations:migrate -n
-php app/console doctrine:fixtures:load -n
+php app/console doctrine:fixtures:load --append -n
 ./bin/phpunit -c app
