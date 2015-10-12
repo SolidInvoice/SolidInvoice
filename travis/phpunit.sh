4bf59a44185cd9af322f1167b4d004536ab83aed
Pierre du Plessis <pierre@pcservice.co.za>
2015-10-12 22:49:26 +0200

    Travis updates

--- travis/phpunit.sh
+++ travis/phpunit.sh
@@ -2,5 +2,5 @@
 
 mysql -e "drop database csbill"
 php app/console doctrine:database:create -n
-php app/console doctrine:migrations:migrate -n
+php app/console doctrine:migrations:migrate -n -q
 ./bin/phpunit -c app
