09532c88ca64dc7adc62d206a7b32e099dba2c5d
Pierre du Plessis <pierre@pcservice.co.za>
2015-10-26 14:32:52 +0200

    Disable code coverage generation when runnning unit tests outside of travis

--- travis/phpunit.sh
+++ travis/phpunit.sh
@@ -3,4 +3,4 @@
 mysql -e "drop database csbill"
 php app/console doctrine:database:create -n
 php app/console doctrine:migrations:migrate -n -q
-./bin/phpunit -c app
+./bin/phpunit -c app --coverage-clover build/logs/clover.xml
