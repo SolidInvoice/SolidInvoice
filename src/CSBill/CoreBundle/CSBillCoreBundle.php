0e63745557e9f56a2d32ce2710004b4e84a1ec0c
Pierre du Plessis <pierre@pcservice.co.za>
2015-12-22 11:55:36 +0200

    Release 0.6.0

--- src/CSBill/CoreBundle/CSBillCoreBundle.php
+++ src/CSBill/CoreBundle/CSBillCoreBundle.php
@@ -17,7 +17,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class CSBillCoreBundle extends Bundle
 {
-    const VERSION = '0.6.0-dev';
+    const VERSION = '0.6.0';
 
     /**
      * {@inheritdoc}
