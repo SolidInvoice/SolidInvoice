f63984e2d0009ca7b1dd04bcc1df33bbf1f82ce2
Pierre du Plessis <pierre@pcservice.co.za>
2016-08-04 21:45:42 +0200

    Format discount properly in grid

--- src/CSBill/DataGridBundle/Resources/public/js/cell/statuscell.js
+++ src/CSBill/DataGridBundle/Resources/public/js/cell/statuscell.js
@@ -1,20 +1,20 @@
-define(['backgrid', 'lodash', 'status_labels'], function (Backgrid, _, Labels) {
+define(['backgrid', 'lodash', 'status_labels'], function(Backgrid, _, Labels) {
 
-    var statusCell = function (name) {
-	return Backgrid.Cell.extend({
-	    render: function () {
-		this.$el.empty();
-		var rawValue = this.model.get(this.column.get('name'));
-		var formattedValue = this.formatter.fromRaw(rawValue, this.model);
-		this.$el.append(Labels[name][formattedValue]);
-		this.delegateEvents();
-		return this;
-	    }
-	});
+    var statusCell = function(name) {
+        return Backgrid.Cell.extend({
+            render: function() {
+                this.$el.empty();
+                var rawValue = this.model.get(this.column.get('name'));
+                var formattedValue = this.formatter.fromRaw(rawValue, this.model);
+                this.$el.append(Labels[name][formattedValue]);
+                this.delegateEvents();
+                return this;
+            }
+        });
     };
 
-    _.each(Labels, function (values, name) {
-	var cellName = _.startCase(name) + '_statusCell';
-	Backgrid[cellName] = statusCell(name);
+    _.each(Labels, function(values, name) {
+        var cellName = _.startCase(name) + '_statusCell';
+        Backgrid[cellName] = statusCell(name);
     });
 });
