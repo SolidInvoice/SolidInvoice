f7826b609ceae919eb18dbd66b39c2c6de15a387
Pierre du Plessis <pierre@pcservice.co.za>
2016-09-22 09:38:31 +0200

    Bump version

--- src/CSBill/CoreBundle/CSBillCoreBundle.php
+++ src/CSBill/CoreBundle/CSBillCoreBundle.php
@@ -17,7 +17,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class CSBillCoreBundle extends Bundle
 {
-    const VERSION = '1.0.1';
+    const VERSION = '1.0.2-dev';
 
     /**
      * {@inheritdoc}
