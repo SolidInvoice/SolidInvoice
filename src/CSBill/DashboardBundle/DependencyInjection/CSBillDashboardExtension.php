30a98dfe9db19e49c9b6ecd2f77b6a3fb7046a77
Pierre du Plessis <pierre@pcservice.co.za>
2017-02-17 00:27:06 +0200

    Upgrade Symfony Yaml and Config components

--- src/CSBill/DashboardBundle/DependencyInjection/CSBillDashboardExtension.php
+++ src/CSBill/DashboardBundle/DependencyInjection/CSBillDashboardExtension.php
@@ -19,7 +19,7 @@ use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 /**
  * This is the class that loads and manages your bundle configuration.
  *
- * @link http://symfony.com/doc/current/cookbook/bundles/extension.html
+ * @see http://symfony.com/doc/current/cookbook/bundles/extension.html
  */
 class CSBillDashboardExtension extends Extension
 {
@@ -28,9 +28,6 @@ class CSBillDashboardExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $configuration = new Configuration();
-        $this->processConfiguration($configuration, $configs);
-
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
     }
--- src/CSBill/MenuBundle/DependencyInjection/CSBillMenuExtension.php
+++ src/CSBill/MenuBundle/DependencyInjection/CSBillMenuExtension.php
@@ -28,9 +28,6 @@ class CSBillMenuExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $configuration = new Configuration();
-        $this->processConfiguration($configuration, $configs);
-
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
     }
