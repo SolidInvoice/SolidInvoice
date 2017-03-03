908fbf3cae9d3cd70308739ecc3af97356cdd59b
Pierre du Plessis <pierre@pcservice.co.za>
2017-03-03 19:52:28 +0200

    Add unit tests for user forms

--- src/CSBill/UserBundle/Form/Type/ProfileType.php
+++ src/CSBill/UserBundle/Form/Type/ProfileType.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * This file is part of CSBill project.
+ *
+ * (c) 2013-2017 Pierre du Plessis <info@customscripts.co.za>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
 
 /*
  * This file is part of CSBill project.
@@ -9,13 +17,13 @@
  * with this source code in the file LICENSE.
  */
 
-namespace CSBill\UserBundle\Form;
+namespace CSBill\UserBundle\Form\Type;
 
 use FOS\UserBundle\Form\Type\ProfileFormType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 
-class ProfileForm extends AbstractType
+class ProfileType extends AbstractType
 {
     /**
      * @param FormBuilderInterface $builder
---
+++ src/CSBill/UserBundle/Tests/Form/Type/ProfileTypeTest.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * This file is part of CSBill project.
+ *
+ * (c) 2013-2017 Pierre du Plessis <info@customscripts.co.za>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace CSBill\UserBundle\Tests\Form\Type;
+
+use CSBill\CoreBundle\Tests\FormTestCase;
+use CSBill\UserBundle\Entity\User;
+use CSBill\UserBundle\Form\Type\ProfileType;
+use FOS\UserBundle\Form\Type\ProfileFormType;
+use Symfony\Component\Form\PreloadedExtension;
+
+class ProfileTypeTest extends FormTestCase
+{
+    public function testSubmit()
+    {
+        $mobile = $this->faker->phoneNumber;
+
+        $formData = [
+            'mobile' => $mobile,
+        ];
+
+        $object = new User();
+        $object->setMobile($mobile);
+
+        $this->assertFormData(ProfileType::class, $formData, $object);
+    }
+
+    protected function getExtensions()
+    {
+        $type = new ProfileFormType(User::class);
+
+        return [
+            new PreloadedExtension([$type], [])
+        ];
+    }
+}
