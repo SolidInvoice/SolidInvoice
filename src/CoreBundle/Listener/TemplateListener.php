71a508da0ac1672ca6ee88966bffb1d6cb9fd902
Pierre du Plessis <pierre@pcservice.co.za>
2017-03-08 20:41:01 +0200

    Fix CS

--- src/CoreBundle/Listener/TemplateListener.php
+++ src/CoreBundle/Listener/TemplateListener.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 /*
  * This file is part of CSBill project.
  *
- * (c) 2013-2017 Pierre du Plessis <info@customscripts.co.za>
+ * (c) 2013-2016 Pierre du Plessis <info@customscripts.co.za>
  *
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
