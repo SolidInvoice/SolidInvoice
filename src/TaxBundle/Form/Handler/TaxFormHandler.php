e629afa8b518302966af604551cfbcccf46da2b4
Pierre du Plessis <pierre@pcservice.co.za>
2017-05-01 12:44:59 +0200

    Fix CS

--- src/TaxBundle/Form/Handler/TaxFormHandler.php
+++ src/TaxBundle/Form/Handler/TaxFormHandler.php
@@ -66,7 +66,7 @@ class TaxFormHandler implements FormHandlerInterface, FormHandlerSuccessInterfac
     }
 
     /**
-     * {@inheritdoc]
+     * {@inheritdoc}
      */
     public function getResponse(FormRequest $formRequest)
     {
