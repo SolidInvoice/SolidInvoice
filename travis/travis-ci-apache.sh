aa5b17bf55f7c83d9fa905f999ec7ead174db679
Pierre du Plessis <pierre@pcservice.co.za>
2017-05-17 16:37:49 +0200

    Update travis apache config

--- travis/travis-ci-apache.sh
+++ travis/travis-ci-apache.sh
@@ -1,3 +1,6 @@
+sudo apt-get update
+sudo apt-get install apache2 libapache2-mod-fastcgi
+
 # set up php under apache
 if [[ $TRAVIS_PHP_VERSION == 'hhvm' ]]; then
     sudo a2enmod rewrite actions fastcgi alias
@@ -6,7 +9,7 @@ if [[ $TRAVIS_PHP_VERSION == 'hhvm' ]]; then
     sudo service apache2 restart
     hhvm -m daemon -vServer.Type=fastcgi -vServer.Port=9000 -vServer.FixPathInfo=true
 else
-    sudo cp ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf.default ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf
+    sudo cp travis/php-fpm.conf ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.d/www.conf
     sudo a2enmod rewrite actions fastcgi alias
     echo "cgi.fix_pathinfo = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
     ~/.phpenv/versions/$(phpenv version-name)/sbin/php-fpm
