291f44c377f77b600d884b8da71f84fb2ecc47dd
Pierre du Plessis <pierre@pcservice.co.za>
2017-05-17 17:05:02 +0200

    Move config to sites-enabled instead of sites-available

--- travis/travis-ci-apache.sh
+++ travis/travis-ci-apache.sh
@@ -4,8 +4,8 @@ sudo apt-get install apache2 libapache2-mod-fastcgi
 # set up php under apache
 if [[ $TRAVIS_PHP_VERSION == 'hhvm' ]]; then
     sudo a2enmod rewrite actions fastcgi alias
-    sudo cp -f travis/travis-ci-apache-hhvm.conf /etc/apache2/sites-available/default
-    sudo sed -e "s?%TRAVIS_BUILD_DIR%?$(pwd)?g" --in-place /etc/apache2/sites-available/default
+    sudo cp -f travis/travis-ci-apache-hhvm.conf /etc/apache2/sites-enabled/default.conf
+    sudo sed -e "s?%TRAVIS_BUILD_DIR%?$(pwd)?g" --in-place /etc/apache2/sites-enabled/default.conf
     sudo service apache2 restart
     hhvm -m daemon -vServer.Type=fastcgi -vServer.Port=9000 -vServer.FixPathInfo=true
 else
@@ -13,7 +13,7 @@ else
     sudo a2enmod rewrite actions fastcgi alias
     echo "cgi.fix_pathinfo = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
     ~/.phpenv/versions/$(phpenv version-name)/sbin/php-fpm
-    sudo cp -f travis/travis-ci-apache.conf /etc/apache2/sites-available/default
-    sudo sed -e "s?%TRAVIS_BUILD_DIR%?$(pwd)?g" --in-place /etc/apache2/sites-available/default
+    sudo cp -f travis/travis-ci-apache.conf /etc/apache2/sites-enabled/default.conf
+    sudo sed -e "s?%TRAVIS_BUILD_DIR%?$(pwd)?g" --in-place /etc/apache2/sites-enabled/.conf
     sudo service apache2 restart
 fi
