1d32fe40442f826a9d334cf984554eff00721ad7
Pierre du Plessis <pierre@pcservice.co.za>
2017-06-07 21:30:08 +0200

    Remove un-used method

--- src/SettingsBundle/Repository/SettingsRepository.php
+++ src/SettingsBundle/Repository/SettingsRepository.php
@@ -13,46 +13,14 @@ declare(strict_types=1);
 
 namespace CSBill\SettingsBundle\Repository;
 
-use CSBill\CoreBundle\Util\ArrayUtil;
 use Doctrine\ORM\EntityRepository;
 
 class SettingsRepository extends EntityRepository
 {
     /**
-     * Gets section specific settings.
-     *
-     * @param string|Section $section
-     * @param bool           $combineArray Should the settings be returned as a key => value array
-     *
-     * @return array
+     * @param array $settings
      */
-    public function getSettingsBySection($section, bool $combineArray = true): array
-    {
-        $qb = $this->createQueryBuilder('s')
-            ->where('s.section = :section')
-            ->orderBy('s.key', 'ASC')
-            ->setParameter('section', $section);
-
-        $query = $qb->getQuery()
-            ->useQueryCache(true);
-
-        $result = $query->getResult();
-
-        if (count($result) > 0 && $combineArray) {
-            try {
-                return array_combine(ArrayUtil::column($result, 'key', false), ArrayUtil::column($result, 'value', false));
-            } catch (\Exception $e) {
-                var_dump(ArrayUtil::column($result, 'key'));
-                var_dump(ArrayUtil::column($result, 'value'));
-                var_dump($result);
-                exit;
-            }
-        }
-
-        return $result;
-    }
-
-    public function save(array $settings)
+    public function save(array $settings): void
     {
         foreach ($settings as $key => $value) {
             $this->createQueryBuilder('s')
