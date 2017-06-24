be2a48da022d392b53c1b4cac5a101b992c079cb
Pierre du Plessis <pierre@pcservice.co.za>
2017-06-24 17:17:40 +0200

    remove -q flag

--- travis/behat.sh
+++ travis/behat.sh
@@ -5,15 +5,15 @@ echo 'Installation...' && echo -en 'travis_fold:start:installation\\r'
 echo -en 'travis_fold:end:installation\\r'
 
 echo 'Login...' && echo -en 'travis_fold:start:login\\r'
-./bin/console doctrine:database:drop --force -n -q
-./bin/console doctrine:database:create -n -q
-./bin/console doctrine:migrations:migrate -n -q
+./bin/console doctrine:database:drop --force -n
+./bin/console doctrine:database:create -n
+./bin/console doctrine:migrations:migrate -n
 ./bin/behat --suite=login -n -f progress -p "$TEST_SUITE" -vvv
 echo -en 'travis_fold:end:login\\r'
 
 echo 'API...' && echo -en 'travis_fold:start:api\\r'
-./bin/console doctrine:database:drop --force -n -q
-./bin/console doctrine:database:create -n -q
-./bin/console doctrine:migrations:migrate -n -q
+./bin/console doctrine:database:drop --force -n
+./bin/console doctrine:database:create -n
+./bin/console doctrine:migrations:migrate -n
 ./bin/behat --suite=api -n -f progress -p "$TEST_SUITE" -vvv
 \echo -en 'travis_fold:end:api\\r'
