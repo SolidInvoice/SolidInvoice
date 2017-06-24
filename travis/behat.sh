483ab122b4532b45f1155958843936a0c1456f88
Pierre du Plessis <pierre@pcservice.co.za>
2017-06-24 17:29:13 +0200

    Revert changes to behat script

--- travis/behat.sh
+++ travis/behat.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-echo 'Installation...' && echo -en 'travis_fold:start:installation\\r'
-./bin/behat --suite=installation -n -f progress -p "$TEST_SUITE" -vvv
-echo -en 'travis_fold:end:installation\\r'
-
-echo 'Login...' && echo -en 'travis_fold:start:login\\r'
-./bin/console doctrine:database:drop --force -n
-./bin/console doctrine:database:create -n
-./bin/console doctrine:migrations:migrate -n
-./bin/behat --suite=login -n -f progress -p "$TEST_SUITE" -vvv
-echo -en 'travis_fold:end:login\\r'
-
-echo 'API...' && echo -en 'travis_fold:start:api\\r'
-./bin/console doctrine:database:drop --force -n
-./bin/console doctrine:database:create -n
-./bin/console doctrine:migrations:migrate -n
-./bin/behat --suite=api -n -f progress -p "$TEST_SUITE" -vvv
-\echo -en 'travis_fold:end:api\\r'
+./bin/behat --suite=installation -n -f progress -p "$TEST_SUITE"
+./bin/behat --suite=login -n -f progress -p "$TEST_SUITE"
+./bin/behat --suite=api -n -f progress -p "$TEST_SUITE"
