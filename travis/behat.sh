252bba515669ef65ded9acef837331848c11ede1
Pierre du Plessis <pierre@pcservice.co.za>
2017-06-24 16:19:08 +0200

    Add verbose output

--- travis/behat.sh
+++ travis/behat.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-./bin/behat --suite=installation -n -f progress -p "$TEST_SUITE"
-./bin/behat --suite=login -n -f progress -p "$TEST_SUITE"
-./bin/behat --suite=api -n -f progress -p "$TEST_SUITE"
+./bin/behat --suite=installation -n -f progress -p "$TEST_SUITE" -vvv
+./bin/behat --suite=login -n -f progress -p "$TEST_SUITE" -vvv
+./bin/behat --suite=api -n -f progress -p "$TEST_SUITE" -vvv
