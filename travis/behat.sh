6f5da1ee2a9fff3b76dcba89800bb8cb2879f1df
Pierre du Plessis <pierre@pcservice.co.za>
2017-06-24 16:48:25 +0200

    Simplify installation process and clear DB before each behat test run

--- travis/behat.sh
+++ travis/behat.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+echo 'Installation...' && echo -en 'travis_fold:start:installation\\r'
 ./bin/behat --suite=installation -n -f progress -p "$TEST_SUITE" -vvv
+echo -en 'travis_fold:end:installation\\r'
+
+echo 'Login...' && echo -en 'travis_fold:start:login\\r'
+./bin/console doctrine:database:drop --force -n -q
+./bin/console doctrine:database:create -n -q
+./bin/console doctrine:migrations:migrate -n -q
 ./bin/behat --suite=login -n -f progress -p "$TEST_SUITE" -vvv
+echo -en 'travis_fold:end:login\\r'
+
+echo 'API...' && echo -en 'travis_fold:start:api\\r'
+./bin/console doctrine:database:drop --force -n -q
+./bin/console doctrine:database:create -n -q
+./bin/console doctrine:migrations:migrate -n -q
 ./bin/behat --suite=api -n -f progress -p "$TEST_SUITE" -vvv
+\echo -en 'travis_fold:end:api\\r'
