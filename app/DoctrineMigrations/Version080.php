2f6de4494e84acf490f1403afd21d21c3898ee4b
Pierre du Plessis <pierre@pcservice.co.za>
2017-07-16 13:25:40 +0200

    Apply fixes from StyleCI (#180)

--- app/DoctrineMigrations/Version080.php
+++ app/DoctrineMigrations/Version080.php
@@ -39,7 +39,7 @@ class Version080 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-        
+
         $this->addSql('DROP INDEX UNIQ_4FABF9833D4E91C8 ON payment_methods');
         $this->addSql('ALTER TABLE payment_methods ADD payment_method VARCHAR(125) NOT NULL COLLATE utf8_unicode_ci, DROP gateway_name, DROP factory, CHANGE config settings LONGTEXT DEFAULT NULL COLLATE utf8_unicode_ci COMMENT \'(DC2Type:array)\'');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_4FABF9837B61A1F6 ON payment_methods (payment_method)');
