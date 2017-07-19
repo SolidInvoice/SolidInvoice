2a3865fbba05b5dff8d85be10ab6bd8ecbcaf454
Pierre du Plessis <pierre@pcservice.co.za>
2017-07-19 20:42:01 +0200

    Re-use Address::fromArray function to render an address

--- src/SettingsBundle/Twig/Extension/SettingsExtension.php
+++ src/SettingsBundle/Twig/Extension/SettingsExtension.php
@@ -13,11 +13,11 @@ declare(strict_types=1);
 
 namespace CSBill\SettingsBundle\Twig\Extension;
 
+use CSBill\ClientBundle\Entity\Address;
 use CSBill\SettingsBundle\Exception\InvalidSettingException;
 use CSBill\SettingsBundle\SystemConfig;
 use Doctrine\DBAL\Exception\ConnectionException;
 use Doctrine\DBAL\Exception\TableNotFoundException;
-use Symfony\Component\Intl\Intl;
 
 class SettingsExtension extends \Twig_Extension
 {
@@ -41,25 +41,7 @@ class SettingsExtension extends \Twig_Extension
 
     public function renderAddress(array $address)
     {
-        static $countries;
-
-        if (empty($countries)) {
-            $countries = Intl::getRegionBundle()->getCountryNames();
-        }
-
-        $info = [
-            $address['street1'] ?? null,
-            $address['street2'] ?? null,
-            $address['city'] ?? null,
-            $address['state'] ?? null,
-            $address['zip'] ?? null,
-        ];
-
-        if (!empty($address['country'])) {
-            $info[] = $countries[$address['country']] ?? null;
-        }
-
-        return trim(implode("\n", array_filter($info)), ', \t\n\r\0\x0B');
+        return (string) Address::fromArray($address);
     }
 
     public function getSetting(string $setting, $default = null, $decode = false)
