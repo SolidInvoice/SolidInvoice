ed1ace695868e1aaf6b5a828c8ea00c53dec5528
Pierre du Plessis <pierre@pcservice.co.za>
2017-08-10 15:18:45 +0200

    Fix serialization on invoice create screen

--- src/CoreBundle/Resources/public/js/billing/model/row_model.js
+++ src/CoreBundle/Resources/public/js/billing/model/row_model.js
@@ -2,10 +2,6 @@ define(['backbone'], function (Backbone) {
     return Backbone.Model.extend({
         defaults: {
             fields: {},
-            description: '',
-            price: 0,
-            qty: 1,
-            total: 0
         }
     });
 });
---
+++ src/CoreBundle/Twig/Extension/SerializerExtension.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of CSBill project.
+ *
+ * (c) 2013-2017 Pierre du Plessis <info@customscripts.co.za>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace CSBill\CoreBundle\Twig\Extension;
+
+use Symfony\Component\Serializer\SerializerInterface;
+
+/**
+ * @codeCoverageIgnore
+ */
+class SerializerExtension extends \Twig_Extension
+{
+    /**
+     * @var SerializerInterface
+     */
+    private $serializer;
+
+    public function __construct(SerializerInterface $serializer)
+    {
+        $this->serializer = $serializer;
+    }
+
+    public function getFilters()
+    {
+        return [
+            new \Twig_SimpleFilter('serialize', function ($data, string $format, array $groups = []) {
+                return $this->serializer->serialize($data, $format, ['groups' => $groups]);
+            }),
+        ];
+    }
+}
