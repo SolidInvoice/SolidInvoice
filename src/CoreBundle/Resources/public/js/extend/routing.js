e7beb942615b201964a300d872824fa462e99f39
Pierre du Plessis <pierre@pcservice.co.za>
2018-09-03 08:52:28 +0200

    Fix Routing script

--- src/CoreBundle/Resources/public/js/extend/routing.js
+++ src/CoreBundle/Resources/public/js/extend/routing.js
@@ -1,8 +1,8 @@
 define(['lodash', 'fos_router', 'fos_routing_data'], function(_, Router, data) {
 
     if (!_.isUndefined(data)) {
-        Router.setData(data);
+        Router.setRoutingData(data);
     }
 
-    return Routing;
+    return Router;
 });
