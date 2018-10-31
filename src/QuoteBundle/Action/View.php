eafc3daef495e4144bd76116d3235a64b6c70c18
Pierre du Plessis <pierre@pcservice.co.za>
2018-10-31 16:21:45 +0200

    Remove debug code when viewing a quote

--- src/QuoteBundle/Action/View.php
+++ src/QuoteBundle/Action/View.php
@@ -14,17 +14,13 @@ declare(strict_types=1);
 namespace SolidInvoice\QuoteBundle\Action;
 
 use SolidInvoice\CoreBundle\Templating\Template;
-use SolidInvoice\MailerBundle\Mailer;
-use SolidInvoice\QuoteBundle\Email\QuoteEmail;
 use SolidInvoice\QuoteBundle\Entity\Quote;
 use Symfony\Component\HttpFoundation\Request;
 
 final class View
 {
-    public function __invoke(Request $request, Mailer $mailer, Quote $quote)
+    public function __invoke(Request $request, Quote $quote)
     {
-        $mailer->send(new QuoteEmail($quote));
-
         return new Template('@SolidInvoiceQuote/Default/view.html.twig', ['quote' => $quote]);
     }
 }
