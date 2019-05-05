c4c4df76f35b2e7e5e5117456bd52ee30cfd386b
Pierre du Plessis <pierre@pcservice.co.za>
2019-05-05 21:29:08 +0200

    Fix incorrect default theme name when running migrations

---
+++ app/DoctrineMigrations/Version20001.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of SolidInvoice project.
+ *
+ * (c) 2013-2017 Pierre du Plessis <info@customscripts.co.za>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\DBAL\Schema\Table;
+use Doctrine\Migrations\AbstractMigration;
+use SolidInvoice\SettingsBundle\SystemConfig;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+
+final class Version20001 extends AbstractMigration implements ContainerAwareInterface
+{
+    use ContainerAwareTrait;
+
+    public function up(Schema $schema): void
+    {
+        /** @var SystemConfig $config */
+        $config = $this->container->get('settings');
+
+        if ('skin-solidinoice-default' === $config->get('design/system/theme')) {
+            $config->set('design/system/theme', 'skin-solidinvoice-default');
+        }
+    }
+
+    public function down(Schema $schema): void
+    {
+    }
+}
