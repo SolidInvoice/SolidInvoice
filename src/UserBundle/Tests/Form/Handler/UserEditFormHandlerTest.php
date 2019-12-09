6d5df45f005628a388b065166f10132e60ad7149
Pierre du Plessis <pierre@pcservice.co.za>
2019-12-09 12:34:08 +0200

    Update User form handler tests

--- src/UserBundle/Tests/Form/Handler/UserEditFormHandlerTest.php
+++ src/UserBundle/Tests/Form/Handler/UserEditFormHandlerTest.php
@@ -14,46 +14,68 @@ declare(strict_types=1);
 namespace SolidInvoice\UserBundle\Tests\Form\Handler;
 
 use Mockery as M;
+use SolidInvoice\CoreBundle\Response\FlashResponse;
 use SolidInvoice\CoreBundle\Templating\Template;
 use SolidInvoice\FormBundle\Test\FormHandlerTestCase;
 use SolidInvoice\UserBundle\Entity\User;
 use SolidInvoice\UserBundle\Form\Handler\UserEditFormHandler;
 use SolidWorx\FormHandler\FormRequest;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\RouterInterface;
-use Symfony\Component\Security\Core\Encoder\BCryptPasswordEncoder;
-use Symfony\Component\Security\Core\Encoder\EncoderFactory;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoder;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserEditFormHandlerTest extends FormHandlerTestCase
 {
     private $router;
 
+    private $userPasswordEncoder;
+
+    private $password;
+
+    private $user;
+
     protected function setUp(): void
     {
         parent::setUp();
 
+        $this->user = new User();
+        $this->password = $this->faker->password;
         $this->router = M::mock(RouterInterface::class);
-
-        $this->router->shouldReceive('generate')
-            ->zeroOrMoreTimes()
-            ->with('_users_list')
-            ->andReturn('/users');
+        $this->userPasswordEncoder = M::mock(UserPasswordEncoderInterface::class);
     }
 
     public function getHandler()
     {
-        $handler = new UserEditFormHandler(new UserPasswordEncoder(new EncoderFactory([User::class => new BCryptPasswordEncoder(10)])), $this->router);
+        $handler = new UserEditFormHandler($this->userPasswordEncoder, $this->router);
         $handler->setDoctrine($this->registry);
 
         return $handler;
     }
 
+    protected function beforeSuccess($data, FormRequest $form)
+    {
+        $this->userPasswordEncoder->shouldReceive('encodePassword')
+            ->once()
+            ->with($data, $this->password)
+            ->andReturn(password_hash($this->password, PASSWORD_DEFAULT));
+
+        $this->router->shouldReceive('generate')
+            ->once()
+            ->with('_users_list')
+            ->andReturn('/users');
+    }
+
     protected function assertOnSuccess(?Response $response, $data, FormRequest $form)
     {
         $this->assertCount(1, $this->em->getRepository(User::class)->findAll());
-        $this->assertInstanceOf(Response::class, $response);
+        $this->assertInstanceOf(RedirectResponse::class, $response);
+        $this->assertInstanceOf(FlashResponse::class, $response);
+        $this->assertSame('/users', $response->getTargetUrl());
         $this->assertSame('test', $data->getUserName());
+        $this->assertTrue(password_verify($this->password, $data->getPassword()));
+        $this->assertCount(1, $response->getFlash());
+        $this->assertSame(FlashResponse::FLASH_SUCCESS, $response->getFlash()->key());
     }
 
     protected function assertResponse(FormRequest $formRequest)
@@ -63,28 +85,25 @@ class UserEditFormHandlerTest extends FormHandlerTestCase
 
     protected function getHandlerOptions(): array
     {
-        $user = new User();
-        $user->setUsername('one');
-        $user->setPassword('one');
-        $user->setPlainPassword('one');
-        $user->setEmail('one@two.com');
+        $this->user->setUsername('one');
+        $this->user->setPassword('one');
+        $this->user->setPlainPassword($this->password);
+        $this->user->setEmail('one@two.com');
 
         return [
-            'user' => $user,
+            'user' => $this->user,
         ];
     }
 
     public function getFormData(): array
     {
-        $password = $this->faker->password;
-
         return [
-            'fos_user_registration_form' => [
+            'user' => [
                 'username' => 'test',
                 'email' => $this->faker->email,
                 'plainPassword' => [
-                    'first' => $password,
-                    'second' => $password,
+                    'first' => $this->password,
+                    'second' => $this->password,
                 ],
                 'mobile' => $this->faker->phoneNumber,
                 'enabled' => true,
