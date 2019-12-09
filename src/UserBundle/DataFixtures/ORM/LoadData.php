da3fcd377b30b7a9f394a890224daf418e306371
Pierre du Plessis <pierre@pcservice.co.za>
2019-12-09 13:35:15 +0200

    Add tests for USerRepository

---
+++ src/UserBundle/DataFixtures/ORM/LoadData.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of SolidInvoice project.
+ *
+ * (c) 2013-2017 Pierre du Plessis <info@customscripts.co.za>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace SolidInvoice\UserBundle\DataFixtures\ORM;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use SolidInvoice\ClientBundle\Entity\Client;
+use SolidInvoice\ClientBundle\Entity\Contact;
+use SolidInvoice\UserBundle\Entity\User;
+
+/**
+ * @codeCoverageIgnore
+ */
+class LoadData extends Fixture
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        $user1 = (new User())
+            ->setUsername('test1')
+            ->setEmail('test1@test.com')
+            ->setPassword('test1')
+            ->setConfirmationToken(base64_encode(bin2hex(random_bytes(24))))
+            ->setPasswordRequestedAt(new \DateTime());
+
+        $user2 = (new User())
+            ->setUsername('test2')
+            ->setEmail('test2@test.com')
+            ->setPassword('test2')
+            ->setEnabled(true);
+
+        $manager->persist($user1);
+        $manager->persist($user2);
+        $manager->flush();
+
+        $this->setReference('user1', $user1);
+        $this->setReference('user2', $user2);
+    }
+}
