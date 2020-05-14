fc5a2b5015a8f948c29cd03f5c96e3feff37f5a5
Pierre du Plessis <pierre@pcservice.co.za>
2020-05-14 09:26:20 +0200

    Skip test bootstrap on travis

--- travis/phpunit.sh
+++ travis/phpunit.sh
@@ -5,6 +5,6 @@ set -o history -o histexpand
 
 SOLIDINVOICE_ENV=test SOLIDINVOICE_DEBUG=0 php bin/console doctrine:migrations:migrate -n -q
 
-./bin/simple-phpunit --coverage-clover build/logs/clover.xml --exclude-group installation,functional
+SKIP_FUNCTIONAL_BOOTSTRAP=1 ./bin/simple-phpunit --coverage-clover build/logs/clover.xml --exclude-group installation,functional
 
 exit $?
