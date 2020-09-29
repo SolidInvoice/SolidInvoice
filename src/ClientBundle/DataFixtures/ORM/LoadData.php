c9e732692558811ef935e6202c894219d2799cb1
Pierre du Plessis <pierre@pcservice.co.za>
2020-09-29 22:03:54 +0200

    Update PHPStan (#333)

--- src/ClientBundle/DataFixtures/ORM/LoadData.php
+++ src/ClientBundle/DataFixtures/ORM/LoadData.php
@@ -14,7 +14,7 @@ declare(strict_types=1);
 namespace SolidInvoice\ClientBundle\DataFixtures\ORM;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Persistence\ObjectManager;
 use SolidInvoice\ClientBundle\Entity\Client;
 use SolidInvoice\ClientBundle\Entity\Contact;
 
--- src/InvoiceBundle/DataFixtures/ORM/LoadData.php
+++ src/InvoiceBundle/DataFixtures/ORM/LoadData.php
@@ -14,7 +14,7 @@ declare(strict_types=1);
 namespace SolidInvoice\InvoiceBundle\DataFixtures\ORM;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Persistence\ObjectManager;
 use Money\Currency;
 use Money\Money;
 use SolidInvoice\InvoiceBundle\Entity\Invoice;
