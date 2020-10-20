8c10b40d766aa0609b8875404dc7d4f69b66ed2a
Pierre du Plessis <pierre@pcservice.co.za>
2020-10-20 22:04:35 +0200

    Update Rector config to add Symfony sets and additional config

--- rector.php
+++ rector.php
@@ -30,12 +30,26 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         SetList::DOCTRINE_DBAL_30,
         SetList::DOCTRINE_SERVICES,
         SetList::DOCTRINE_CODE_QUALITY,
+
+        // Symfony
+        SetList::SYMFONY_40,
+        SetList::SYMFONY_41,
+        SetList::SYMFONY_42,
+        SetList::SYMFONY_43,
+        SetList::SYMFONY_44,
+        SetList::SYMFONY_50,
+        SetList::SYMFONY_CODE_QUALITY,
+        SetList::SYMFONY_CONSTRUCTOR_INJECTION,
     ]);
 
     $parameters->set(Option::PATHS, __DIR__.'/src');
     $parameters->set(Option::OPTION_AUTOLOAD_FILE, __DIR__.'/app/autoload.php');
+    $parameters->set(Option::AUTOLOAD_PATHS, __DIR__.'/vendor/bin/.phpunit/phpunit-8.3-0/vendor/autoload.php');
+
     $parameters->set(Option::AUTO_IMPORT_NAMES, true);
     $parameters->set(Option::IMPORT_SHORT_CLASSES, false);
     $parameters->set(Option::IMPORT_DOC_BLOCKS, false);
     $parameters->set(Option::SYMFONY_CONTAINER_XML_PATH_PARAMETER, __DIR__.'/var/cache/dev/appAppKernelDevDebugContainer.xml');
+    $parameters->set(Option::PHP_VERSION_FEATURES, '7.3');
+    $parameters->set(Option::PROJECT_TYPE, Option::PROJECT_TYPE_OPEN_SOURCE);
 };
