c50702d6301b6d1e44bc65f7f9557150472d59f8
Pierre du Plessis <pierre@pcservice.co.za>
2020-11-22 20:34:33 +0200

    Add mailer config factory

---
+++ src/MailerBundle/DependencyInjection/CompilerPass/MailerTransportConfigCompilerPass.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the SolidInvoice project.
+ *
+ * @author     pierre
+ * @copyright  Copyright (c) 2020
+ */
+
+namespace SolidInvoice\MailerBundle\DependencyInjection\CompilerPass;
+
+use SolidInvoice\MailerBundle\Factory\MailerConfigFactory;
+use Symfony\Component\DependencyInjection\Argument\TaggedIteratorArgument;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Reference;
+
+final class MailerTransportConfigCompilerPass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $container): void
+    {
+        if (!$container->hasDefinition('mailer.transport_factory')) {
+            return;
+        }
+
+        $definition = new Definition(MailerConfigFactory::class);
+        $definition->setDecoratedService('mailer.transport_factory');
+        $definition->addArgument(new Reference(MailerConfigFactory::class.'.inner'));
+        $definition->setArgument('$transports', new TaggedIteratorArgument('solidinvoice_mailer.transport.configurator'));
+        $definition->setAutowired(true);
+        $container->setDefinition(MailerConfigFactory::class, $definition);
+    }
+}
