c66a3b08a5547b9222dca23b582d2bf799c4187b
Pierre du Plessis <pierre@pcservice.co.za>
2020-11-24 07:12:17 +0200

    Update tests in InvoiceBundle

---
+++ src/InvoiceBundle/Tests/Listener/Mailer/InvoiceMailerListenerTest.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of SolidInvoice project.
+ *
+ * (c) 2013-2017 Pierre du Plessis <info@customscripts.co.za>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace SolidInvoice\InvoiceBundle\Tests\Listener\Mailer;
+
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use Mockery as M;
+use PHPUnit\Framework\TestCase;
+use SolidInvoice\ClientBundle\Entity\Contact;
+use SolidInvoice\InvoiceBundle\Entity\Invoice;
+use SolidInvoice\InvoiceBundle\Event\InvoiceEvent;
+use SolidInvoice\InvoiceBundle\Event\InvoiceEvents;
+use SolidInvoice\InvoiceBundle\Listener\Mailer\InvoiceMailerListener;
+use Symfony\Component\Mailer\MailerInterface;
+
+class InvoiceMailerListenerTest extends TestCase
+{
+    use MockeryPHPUnitIntegration;
+
+    public function testListener(): void
+    {
+        $invoice = new Invoice();
+
+        $mailer = M::mock(MailerInterface::class);
+        $mailer->shouldReceive('send');
+
+        $listener = new InvoiceMailerListener($mailer);
+
+        $invoice->addUser((new Contact())->setEmail('another@example.com')->setFirstName('Another'));
+        $listener->onInvoiceAccepted(new InvoiceEvent($invoice));
+    }
+
+    public function testEvents(): void
+    {
+        self::assertSame([InvoiceEvents::INVOICE_POST_ACCEPT], \array_keys(InvoiceMailerListener::getSubscribedEvents()));
+    }
+}
