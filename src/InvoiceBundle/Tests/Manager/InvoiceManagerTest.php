98f098c22d7d9dce8f284bf93bd3c2e33e93277c
Pierre du Plessis <pierre@pcservice.co.za>
2020-12-16 08:09:38 +0200

    Update unit tests

--- src/InvoiceBundle/Tests/Manager/InvoiceManagerTest.php
+++ src/InvoiceBundle/Tests/Manager/InvoiceManagerTest.php
@@ -24,6 +24,7 @@ use Money\Money;
 use SolidInvoice\ClientBundle\Entity\Client;
 use SolidInvoice\CoreBundle\Entity\Discount;
 use SolidInvoice\InvoiceBundle\Entity\Item as InvoiceItem;
+use SolidInvoice\InvoiceBundle\Entity\RecurringInvoice;
 use SolidInvoice\InvoiceBundle\Listener\WorkFlowSubscriber;
 use SolidInvoice\InvoiceBundle\Manager\InvoiceManager;
 use SolidInvoice\NotificationBundle\Notification\NotificationManager;
@@ -127,6 +128,67 @@ class InvoiceManagerTest extends KernelTestCase
         static::assertNull($invoice->getStatus());
 
         static::assertNotSame($quote->getUuid(), $invoice->getUuid());
+        static::assertNull($invoice->getId());
+
+        static::assertCount(1, $invoice->getItems());
+
+        $invoiceItem = $invoice->getItems();
+        static::assertInstanceOf(InvoiceItem::class, $invoiceItem[0]);
+
+        static::assertSame($item->getTax(), $invoiceItem[0]->getTax());
+        static::assertSame($item->getDescription(), $invoiceItem[0]->getDescription());
+        static::assertInstanceOf('DateTime', $invoiceItem[0]->getCreated());
+        static::assertEquals($item->getPrice(), $invoiceItem[0]->getPrice());
+        static::assertSame($item->getQty(), $invoiceItem[0]->getQty());
+    }
+
+    public function testCreateFromRecurring()
+    {
+        $currency = new Currency('USD');
+
+        $client = new Client();
+        $client->setName('Test Client');
+        $client->setWebsite('http://example.com');
+        $client->setCreated(new DateTime('NOW'));
+
+        $tax = new Tax();
+        $tax->setName('VAT');
+        $tax->setRate(14.00);
+        $tax->setType(Tax::TYPE_INCLUSIVE);
+
+        $item = new InvoiceItem();
+        $item->setTax($tax);
+        $item->setDescription('Item Description');
+        $item->setCreated(new DateTime('now'));
+        $item->setPrice(new Money(120, $currency));
+        $item->setQty(10);
+        $item->setTotal(new Money((12 * 10), $currency));
+
+        $recurringInvoice = new RecurringInvoice();
+        $recurringInvoice->setBaseTotal(new Money(123, $currency));
+        $discount = new Discount();
+        $discount->setType(Discount::TYPE_PERCENTAGE);
+        $discount->setValue(12);
+        $recurringInvoice->setDiscount($discount);
+        $recurringInvoice->setNotes('Notes');
+        $recurringInvoice->setTax(new Money(432, $currency));
+        $recurringInvoice->setTerms('Terms');
+        $recurringInvoice->setTotal(new Money(987, $currency));
+        $recurringInvoice->setClient($client);
+        $recurringInvoice->addItem($item);
+        $recurringInvoice->setFrequency('* 0 0 * *');
+
+        $invoice = $this->manager->createFromRecurring($recurringInvoice);
+
+        static::assertEquals($recurringInvoice->getTotal(), $invoice->getTotal());
+        static::assertEquals($recurringInvoice->getBaseTotal(), $invoice->getBaseTotal());
+        static::assertSame($recurringInvoice->getDiscount(), $invoice->getDiscount());
+        static::assertSame($recurringInvoice->getNotes(), $invoice->getNotes());
+        static::assertSame($recurringInvoice->getTerms(), $invoice->getTerms());
+        static::assertEquals($recurringInvoice->getTax(), $invoice->getTax());
+        static::assertSame($client, $invoice->getClient());
+        static::assertNull($invoice->getStatus());
+
         static::assertNull($invoice->getId());
 
         static::assertCount(1, $invoice->getItems());
