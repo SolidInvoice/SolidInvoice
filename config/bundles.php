305be4ea9d2ea43934f98b2bd7da7d8d254bcec7
Pierre du Plessis <pierre@pcservice.co.za>
2021-03-22 21:27:32 +0200

    Fix CS

--- config/bundles.php
+++ config/bundles.php
@@ -8,6 +8,7 @@
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
  */
+
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
