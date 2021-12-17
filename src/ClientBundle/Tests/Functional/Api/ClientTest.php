f63151d32c2d13e09268772acde32c69d9fd7f75
Pierre du Plessis <pierre@pcservice.co.za>
2021-12-17 12:22:57 +0200

    Fix ids in test case

--- src/ClientBundle/Tests/Functional/Api/ClientTest.php
+++ src/ClientBundle/Tests/Functional/Api/ClientTest.php
@@ -49,7 +49,7 @@ class ClientTest extends ApiTestCase
         $result = $this->requestPost('/api/clients', $data);
 
         static::assertSame([
-            'id' => 1,
+            'id' => 2,
             'name' => 'Dummy User',
             'website' => null,
             'status' => 'active',
@@ -57,7 +57,7 @@ class ClientTest extends ApiTestCase
             'vatNumber' => null,
             'contacts' => [
                 [
-                    'id' => 1,
+                    'id' => 2,
                     'firstName' => 'foo bar',
                     'lastName' => null,
                     'email' => 'foo@example.com',
