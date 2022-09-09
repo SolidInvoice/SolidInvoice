e813220836af4292fbddba151030c49cfbcd14da
Pierre du Plessis <pierre@pcservice.co.za>
2022-09-09 15:31:41 +0200

    Bump version for dev

--- src/CoreBundle/SolidInvoiceCoreBundle.php
+++ src/CoreBundle/SolidInvoiceCoreBundle.php
@@ -19,7 +19,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class SolidInvoiceCoreBundle extends Bundle
 {
-    public const VERSION = '2.2.0-dev';
+    public const VERSION = '2.1.1-dev';
 
     public const APP_NAME = 'SolidInvoice';
 
