158a5790f25969d80ad6a0d5d79d44d0ea56e390
Pierre du Plessis <pierre@pcservice.co.za>
2022-10-03 10:01:56 +0200

    Fix CS

--- tests/stub.php
+++ tests/stub.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of SolidInvoice project.
+ *
+ * (c) Pierre du Plessis <open-source@solidworx.co>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace Symfony\Component\DependencyInjection;
 
 interface ContainerInterface
@@ -28,5 +37,4 @@ class KernelTestCase
      * @var ContainerInterface|null
      */
     protected static $container;
-
 }
