477361895d45aa1ef43f44f8cce5e0191b940b30
Pierre du Plessis <pierre@pcservice.co.za>
2022-12-01 10:00:39 +0200

    Redirect to system setup when application has no users

--- src/InstallBundle/Listener/RequestListener.php
+++ src/InstallBundle/Listener/RequestListener.php
@@ -14,18 +14,20 @@ declare(strict_types=1);
 namespace SolidInvoice\InstallBundle\Listener;
 
 use SolidInvoice\InstallBundle\Exception\ApplicationInstalledException;
+use SolidInvoice\UserBundle\Repository\UserRepositoryInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\Routing\RouterInterface;
+use function in_array;
 
 /**
  * Listener class to intercept requests
  * and redirect to the installer if necessary.
  */
-class RequestListener implements EventSubscriberInterface
+final class RequestListener implements EventSubscriberInterface
 {
     public const INSTALLER_ROUTE = '_install_check_requirements';
 
@@ -39,10 +41,17 @@ class RequestListener implements EventSubscriberInterface
     /**
      * @var list<string>
      */
-    private array $installRoutes = [
+    private const INSTALL_ROUTES = [
         self::INSTALLER_ROUTE,
         '_install_config',
         '_install_install',
+        ...self::SETUP_ROUTES
+    ];
+
+    /**
+     * @var list<string>
+     */
+    private const SETUP_ROUTES = [
         '_install_setup',
         '_install_finish',
     ];
@@ -50,7 +59,7 @@ class RequestListener implements EventSubscriberInterface
     /**
      * @var list<string>
      */
-    private array $debugRoutes = [
+    private const DEBUG_ROUTES = [
         '_wdt',
         '_profiler',
         '_profiler_search',
@@ -63,6 +72,10 @@ class RequestListener implements EventSubscriberInterface
 
     private RouterInterface $router;
 
+    private UserRepositoryInterface $userRepository;
+
+    private bool $debug;
+
     public static function getSubscribedEvents(): array
     {
         return [
@@ -70,14 +83,20 @@ class RequestListener implements EventSubscriberInterface
         ];
     }
 
-    public function __construct(?string $installed, RouterInterface $router, bool $debug = false)
-    {
-        $this->installed = $installed;
+    public function __construct(
+        RouterInterface $router,
+        UserRepositoryInterface $userRepository,
+        ?string $installed,
+        bool $debug = false
+    ) {
+        $this->userRepository = $userRepository;
         $this->router = $router;
-        $this->allowRoutes += $this->installRoutes;
+        $this->installed = $installed;
+        $this->allowRoutes = self::INSTALL_ROUTES;
+        $this->debug = $debug;
 
-        if ($debug) {
-            $this->allowRoutes = array_merge($this->allowRoutes, $this->debugRoutes);
+        if ($this->debug) {
+            $this->allowRoutes = array_merge($this->allowRoutes, self::DEBUG_ROUTES);
         }
     }
 
@@ -95,14 +114,31 @@ class RequestListener implements EventSubscriberInterface
         $route = $request->get('_route');
 
         if (null !== $this->installed) {
-            if (in_array($route, $this->installRoutes, true) && ! $session->has('installation_step')) {
+            // If the application is installed, but we don't have any users
+            // Redirect to the setup page
+            if ($this->userRepository->getUserCount() === 0) {
+                if (! in_array($route, [...self::SETUP_ROUTES, ...($this->debug ? self::DEBUG_ROUTES : [])], true)) {
+                    $this->redirectToRoute($event, '_install_setup');
+                }
+
+                return;
+            }
+
+            // If the application is installed, and we already have users, and the installer route is requested
+            // then throw an exception
+            if (in_array($route, self::INSTALL_ROUTES, true) && ! $session->has('installation_step')) {
                 throw new ApplicationInstalledException();
             }
         } elseif (! in_array($route, $this->allowRoutes, true)) {
-            $response = new RedirectResponse($this->router->generate(self::INSTALLER_ROUTE));
-
-            $event->setResponse($response);
-            $event->stopPropagation();
+            $this->redirectToRoute($event, self::INSTALLER_ROUTE);
         }
+    }
+
+    private function redirectToRoute(RequestEvent $event, string $route): void
+    {
+        $response = new RedirectResponse($this->router->generate($route));
+
+        $event->setResponse($response);
+        $event->stopPropagation();
     }
 }
