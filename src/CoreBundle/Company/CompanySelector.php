c818352b4c62d3661748867933a46f83c46315b1
Pierre du Plessis <pierre@pcservice.co.za>
2023-02-24 11:40:20 +0200

    Set company parameter on company filter when switcing company

--- src/CoreBundle/Company/CompanySelector.php
+++ src/CoreBundle/Company/CompanySelector.php
@@ -3,26 +3,38 @@ declare(strict_types=1);
 
 namespace SolidInvoice\CoreBundle\Company;
 
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\RequestStack;
 
 final class CompanySelector
 {
     private RequestStack $requestStack;
+    private ManagerRegistry $registry;
 
-    public function __construct(RequestStack $requestStack)
+    public function __construct(RequestStack $requestStack, ManagerRegistry $registry)
     {
         $this->requestStack = $requestStack;
+        $this->registry = $registry;
     }
 
     public function getCompany(): ?int
     {
-        $session = $this->requestStack->getSession();
-
-        return $session->get('companyId');
+        return $this->requestStack->getSession()->get('companyId');
     }
 
     public function switchCompany(int $companyId): void
     {
+        $em = $this->registry->getManager();
+
+        assert($em instanceof EntityManagerInterface);
+
+        $em
+            ->getFilters()
+            ->enable('company')
+            ->setParameter('companyId', $companyId, Types::INTEGER );
+
         $session = $this->requestStack->getSession();
 
         $session->set('companyId', $companyId);
