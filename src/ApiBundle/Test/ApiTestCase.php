9c4eeef03a873186e72ff2e5254dbb4c514816d8
Pierre du Plessis <pierre@pcservice.co.za>
2023-03-25 12:29:23 +0200

    Set company in ApiTestCase

--- src/ApiBundle/Test/ApiTestCase.php
+++ src/ApiBundle/Test/ApiTestCase.php
@@ -70,6 +70,8 @@ abstract class ApiTestCase extends PantherTestCase
             $users = [$user];
         }
 
+        static::getContainer()->get(CompanySelector::class)->switchCompany($companies[0]->getId());
+
         // @phpstan-ignore-next-line Ignore this line in PHPStan, since it sees the SystemConfig service as private
         static::getContainer()->get(CompanySelector::class)->switchCompany($companies[0]->getId());
 
