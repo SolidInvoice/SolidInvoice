a3131a71328febd3251bc538547ed73fb62060bd
Pierre du Plessis <pierre@pcservice.co.za>
2023-04-19 14:39:56 +0200

    Add event listener to update users last login

---
+++ src/UserBundle/EventSubscriber/UserLoginEventSubscriber.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of SolidInvoice project.
+ *
+ * (c) Pierre du Plessis <open-source@solidworx.co>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace SolidInvoice\UserBundle\EventSubscriber;
+
+use DateTimeImmutable;
+use Doctrine\ORM\EntityManagerInterface;
+use SolidInvoice\UserBundle\Entity\User;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
+
+final class UserLoginEventSubscriber implements EventSubscriberInterface
+{
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * @return array<string, string>
+     */
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            InteractiveLoginEvent::class => 'onLogin',
+        ];
+    }
+
+    public function onLogin(InteractiveLoginEvent $event): void
+    {
+        $user = $event->getAuthenticationToken()->getUser();
+        assert($user instanceof User);
+
+        $user->setLastLogin(new DateTimeImmutable());
+
+        $this->entityManager->getRepository(User::class)->save($user);
+    }
+}
