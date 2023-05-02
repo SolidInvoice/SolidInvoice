c4daea01a234eeb5bfc6c0ea7249ed9ca98c0614
Pierre du Plessis <pierre@pcservice.co.za>
2023-05-02 15:06:08 +0200

    Remove readonly properties in Entity to avoid errors

--- src/UserBundle/Entity/User.php
+++ src/UserBundle/Entity/User.php
@@ -40,7 +40,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface, Stringa
 
     use TimeStampable;
 
-    #[ORM\Column(type: UuidBinaryOrderedTimeType::class)]
+    #[ORM\Column(type: UuidBinaryOrderedTimeType::NAME)]
     #[ORM\Id]
     #[ORM\GeneratedValue(strategy: 'CUSTOM')]
     #[ORM\CustomIdGenerator(class: UuidOrderedTimeGenerator::class)]
