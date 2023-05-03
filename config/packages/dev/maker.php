b3cd6ccda136427ed432eb76603d61b8bbbab761
Pierre du Plessis <pierre@pcservice.co.za>
2023-05-03 14:40:58 +0200

    Move config inspection to test for PHPStan

--- config/packages/dev/maker.php
+++ config/packages/dev/maker.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 use Symfony\Config\MakerConfig;
 
+// @phpstan-ignore-next-line
 return static function (MakerConfig $config): void {
+    // @phpstan-ignore-next-line
     $config->rootNamespace('SolidInvoice');
 };
