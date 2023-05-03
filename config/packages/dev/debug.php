58d863fee47f4110b790894fe7b379f20faf7150
Pierre du Plessis <pierre@pcservice.co.za>
2023-05-03 14:40:58 +0200

    Move config inspection to test for PHPStan

--- config/packages/dev/debug.php
+++ config/packages/dev/debug.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 use Symfony\Config\DebugConfig;
 use function Symfony\Component\DependencyInjection\Loader\Configurator\env;
 
+// @phpstan-ignore-next-line
 return static function (DebugConfig $config): void {
+    // @phpstan-ignore-next-line
     $config->dumpDestination('tcp://' . env('VAR_DUMPER_SERVER'));
 };
