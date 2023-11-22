0927969e69cd49d0a10a9acd56dc64c0186347a7
Pierre du Plessis <pierre@pcservice.co.za>
2023-11-22 15:12:48 +0200

    Check for numeric values

--- src/MoneyBundle/Form/DataTransformer/ViewTransformer.php
+++ src/MoneyBundle/Form/DataTransformer/ViewTransformer.php
@@ -39,7 +39,7 @@ class ViewTransformer implements DataTransformerInterface
 
     public function reverseTransform($value): Money
     {
-        if (null === $value) {
+        if (!is_numeric($value)) {
             $value = 0;
         }
 
