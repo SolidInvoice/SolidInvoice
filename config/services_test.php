72e81d54582015ed8db3b9d4e82fbfee59d485ed
Pierre du Plessis <pierre@pcservice.co.za>
2024-04-30 20:36:06 +0200

    Use doctrine service instead of EntityManagerInterface directly

--- config/services_test.php
+++ config/services_test.php
@@ -2,10 +2,8 @@
 
 declare(strict_types=1);
 
-use Doctrine\ORM\EntityManagerInterface;
 use SolidInvoice\CoreBundle\ConfigWriter;
 use SolidInvoice\InstallBundle\Installer\Database\Migration;
-use SolidInvoice\PaymentBundle\Factory\PaymentFactories;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
@@ -24,8 +22,5 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         ->public();
 
     $services->set(ConfigWriter::class)
-        ->public();
-
-    $services->set(EntityManagerInterface::class)
         ->public();
 };
