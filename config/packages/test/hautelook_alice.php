f1dccddb276d6c603047e119eaca5a4bd06d89db
Pierre du Plessis <pierre@pcservice.co.za>
2024-05-01 14:14:30 +0200

    Remove unneeded config files

--- config/packages/test/hautelook_alice.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->import(dirname(__DIR__, 1) . '/dev/hautelook_alice.php');
-};
