f4c7cff212590d708de830ca9052a5b3559c5c85
Pierre du Plessis <pierre@pcservice.co.za>
2024-05-01 08:47:11 +0200

    Fix linting on scripts

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -4,7 +4,7 @@
 if [ -n "$GITHUB_ACTIONS" ]; then
   git config --local user.email "action@github.com"
   git config --local user.name "GitHub Action"
-  git remote set-url origin https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git
+  git remote set-url origin https://"${GITHUB_ACTOR}":"${GITHUB_TOKEN}"@github.com/"${GITHUB_REPOSITORY}".git
 fi
 
 # File path
@@ -18,11 +18,11 @@ bump_version() {
   local increment=$3
 
   IFS='.' read -ra parts <<< "$version"
-  ((parts[$field]+=increment))
+  ((parts[field]+=increment))
 
-  if [ $field -lt 2 ]; then
+  if [ "$field" -lt 2 ]; then
     for i in $(seq $((field+1)) 2); do
-      parts[$i]=0
+      parts[i]=0
     done
   fi
 
@@ -35,12 +35,12 @@ if [ "$1" ]; then
 else
   current_version=$(awk -F\' '/public const VERSION/ {print $2}' $FILE)
   clean_version="${current_version%-dev}"
-  next_version=$(bump_version $clean_version 2 1)  # bumping minor version
+  next_version=$(bump_version "$clean_version" 2 1)  # bumping minor version
 fi
 
 # Update file with bumped version
 sed -i "s/public const VERSION = '.*';/public const VERSION = '$next_version';/" $FILE
-jq --arg version "$next_version" '.version=$version' --indent 4 $PACKAGE_JSON > tmp.json && mv tmp.json $PACKAGE_JSON
+jq --arg version "$next_version" '.version=$version' --indent 2 $PACKAGE_JSON > tmp.json && mv tmp.json $PACKAGE_JSON
 
 git add $FILE $PACKAGE_JSON
 git commit -m "Release version $next_version"
