af87e1049cc8a2b6254c370d54724e8e84916bb8
Pierre du Plessis <pierre@pcservice.co.za>
2024-05-01 14:12:25 +0200

    Remove unused packages

--- config/packages/dev/hautelook_alice.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use Symfony\Config\HautelookAliceConfig;
-
-return static function (HautelookAliceConfig $config): void {
-    $config->fixturesPath(['fixtures']);
-};
