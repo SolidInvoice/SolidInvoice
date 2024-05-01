6ea34774093b090729007bccfd8aef4855d81388
Pierre du Plessis <pierre@pcservice.co.za>
2024-05-01 15:09:07 +0200

    Create database schema in GH actions before unit tests instead of every time when running unit tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -10,48 +10,9 @@
  */
 
 use Doctrine\Deprecations\Deprecation;
-use SolidInvoice\CoreBundle\Entity\Version;
-use SolidInvoice\CoreBundle\Repository\VersionRepository;
-use SolidInvoice\CoreBundle\SolidInvoiceCoreBundle;
-use SolidInvoice\Kernel;
-use SolidInvoice\SettingsBundle\SystemConfig;
-use Symfony\Bundle\FrameworkBundle\Console\Application;
-use Symfony\Component\Console\Input\ArrayInput;
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
 if (class_exists(Deprecation::class)) {
     Deprecation::enableWithTriggerError();
 }
-
-(static function (): void {
-    $kernel = new Kernel('test', true);
-    $kernel->boot();
-
-    $application = new Application($kernel);
-    $application->setAutoExit(false);
-
-    $application->run(new ArrayInput([
-        'command' => 'doctrine:database:drop',
-        '--if-exists' => '1',
-        '--force' => '1',
-    ]));
-
-    $application->run(new ArrayInput([
-        'command' => 'doctrine:database:create',
-    ]));
-
-    $application->run(new ArrayInput([
-        'command' => 'doctrine:migrations:migrate',
-        '--allow-no-migration' => '1',
-        '--no-interaction' => '1',
-    ]));
-
-    $kernel->getContainer()->get(SystemConfig::class)->set(SystemConfig::CURRENCY_CONFIG_PATH, 'USD');
-
-    /** @var VersionRepository $version */
-    $version = $kernel->getContainer()->get('doctrine')->getManager()->getRepository(Version::class);
-    $version->updateVersion(SolidInvoiceCoreBundle::VERSION);
-
-    $kernel->shutdown();
-})();
