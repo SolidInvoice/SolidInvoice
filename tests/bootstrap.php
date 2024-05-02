aa2b2404931374c6477136d6c7564f04b0670647
Pierre du Plessis <pierre@pcservice.co.za>
2024-05-02 21:37:45 +0200

    Ensure tests are always run with a fresh cache

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -16,3 +16,41 @@ require dirname(__DIR__) . '/vendor/autoload.php';
 if (class_exists(Deprecation::class)) {
     Deprecation::enableWithTriggerError();
 }
+
+if (false === (bool) $_SERVER['SOLIDINVOICE_DEBUG']) {
+    // ensure fresh cache
+    (new Symfony\Component\Filesystem\Filesystem())->remove(__DIR__ . '/../var/cache/test');
+}
+
+/*
+(static function (): void {
+    $kernel = new Kernel('test', true);
+    $kernel->boot();
+
+    $application = new Application($kernel);
+    $application->setAutoExit(false);
+
+    $application->run(new ArrayInput([
+        'command' => 'doctrine:database:drop',
+        '--if-exists' => '1',
+        '--force' => '1',
+    ]));
+
+    $application->run(new ArrayInput([
+        'command' => 'doctrine:database:create',
+    ]));
+
+    $application->run(new ArrayInput([
+        'command' => 'doctrine:migrations:migrate',
+        '--allow-no-migration' => '1',
+        '--no-interaction' => '1',
+    ]));
+
+    $kernel->getContainer()->get(SystemConfig::class)->set(SystemConfig::CURRENCY_CONFIG_PATH, 'USD');
+
+    /** @var VersionRepository $version * /
+    $version = $kernel->getContainer()->get('doctrine')->getManager()->getRepository(Version::class);
+    $version->updateVersion(SolidInvoiceCoreBundle::VERSION);
+
+    $kernel->shutdown();
+})();*/
