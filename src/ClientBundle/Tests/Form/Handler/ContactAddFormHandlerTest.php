7da6e6cf45d76353678c9e5fb9ff44acefc00d5c
Pierre du Plessis <pierre@pcservice.co.za>
2024-10-23 20:55:46 +0200

    Fix test

--- src/ClientBundle/Tests/Form/Handler/ContactAddFormHandlerTest.php
+++ src/ClientBundle/Tests/Form/Handler/ContactAddFormHandlerTest.php
@@ -17,15 +17,45 @@ use SolidInvoice\ClientBundle\Entity\Contact;
 use SolidInvoice\ClientBundle\Form\Handler\ContactAddFormHandler;
 use SolidInvoice\ClientBundle\Test\Factory\ClientFactory;
 use SolidInvoice\CoreBundle\Templating\Template;
-use SolidInvoice\CoreBundle\Test\Traits\SymfonyKernelTrait;
 use SolidInvoice\FormBundle\Test\FormHandlerTestCase;
 use SolidWorx\FormHandler\FormRequest;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Zenstruck\Foundry\ChainManagerRegistry;
+use Zenstruck\Foundry\Factory;
+use Zenstruck\Foundry\Test\Factories;
+use Zenstruck\Foundry\Test\LazyManagerRegistry;
+use Zenstruck\Foundry\Test\TestState;
 
 class ContactAddFormHandlerTest extends FormHandlerTestCase
 {
-    use SymfonyKernelTrait;
+    use Factories;
+
+    protected function setUp(): void
+    {
+        $kernel = static::createKernel();
+        $kernel->boot();
+
+        TestState::bootFromContainer($kernel->getContainer());
+        Factory::configuration()->setManagerRegistry(
+            new LazyManagerRegistry(
+                static function (): ChainManagerRegistry {
+                    if (! static::$booted) {
+                        static::bootKernel();
+                    }
+
+                    return TestState::initializeChainManagerRegistry(static::$kernel->getContainer());
+                },
+            ),
+        );
+
+        $kernel->shutdown();
+
+        parent::setUp();
+
+        $this->firstName = $this->faker->firstName;
+        $this->email = $this->faker->email;
+    }
 
     public function getHandler(): ContactAddFormHandler
     {
@@ -36,9 +66,14 @@ class ContactAddFormHandlerTest extends FormHandlerTestCase
         return $handler;
     }
 
+    /**
+     * @return array{contact: Contact}
+     */
     protected function getHandlerOptions(): array
     {
-        return ['contact' => (new Contact())->setClient(ClientFactory::new()->create()->object())];
+        $client = ClientFactory::createOne()->object();
+
+        return ['contact' => (new Contact())->setClient($client)];
     }
 
     public function getFormData(): array
