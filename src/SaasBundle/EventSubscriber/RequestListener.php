a47685aeb1742853128abadf1403c319e2fb532b
Pierre du Plessis <pierre@pcservice.co.za>
2025-05-26 13:48:50 +0200

    Upate listeners and templates

--- src/SaasBundle/EventSubscriber/RequestListener.php
+++ src/SaasBundle/EventSubscriber/RequestListener.php
@@ -11,15 +11,25 @@
 
 namespace SolidInvoice\SaasBundle\EventSubscriber;
 
+use DateTimeImmutable;
+use DateTimeZone;
 use SolidInvoice\CoreBundle\Company\CompanySelector;
 use SolidInvoice\CoreBundle\Repository\CompanyRepository;
+use SolidInvoice\UserBundle\Entity\User;
+use SolidWorx\Platform\SaasBundle\Entity\Subscription;
 use SolidWorx\Platform\SaasBundle\Enum\SubscriptionStatus;
 use SolidWorx\Platform\SaasBundle\Subscription\SubscriptionManager;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
 use Symfony\Component\Uid\Ulid;
 use Twig\Environment;
+use function assert;
+use function in_array;
+use function str_replace;
 
 final readonly class RequestListener implements EventSubscriberInterface
 {
@@ -43,6 +53,7 @@ final readonly class RequestListener implements EventSubscriberInterface
         private CompanyRepository $companyRepository,
         private SubscriptionManager $subscriptionManager,
         private Environment $twig,
+        private Security $security,
     ) {
     }
 
@@ -50,39 +61,120 @@ final readonly class RequestListener implements EventSubscriberInterface
     {
         return [
             RequestEvent::class => 'onRequest',
+            ResponseEvent::class => 'onResponse',
         ];
     }
 
     public function onRequest(RequestEvent $event): void
     {
-        if (in_array($event->getRequest()->attributes->get('_route'), self::SKIPPED_ROUTES, true)) {
+        $subscription = $this->getSubscription($event->getRequest());
+        if (! $subscription instanceof Subscription) {
             return;
+        }
+
+        switch ($subscription?->getStatus()) {
+            case SubscriptionStatus::PENDING:
+                $user = $this->security->getUser();
+                assert($user instanceof User);
+
+                $checkoutUrl = $this->subscriptionManager->getCheckoutUrl($subscription, ['email' => $user->getEmail()]);
+                $event->setResponse(
+                    new Response(
+                        $this->twig->render('@SolidInvoiceSaas/subscription/pending.html.twig', [
+                            'subscription' => $subscription,
+                            'checkoutUrl' => $checkoutUrl,
+                        ]),
+                    )
+                );
+                break;
+            case SubscriptionStatus::CANCELLED:
+            case SubscriptionStatus::EXPIRED:
+                if ($subscription->getEndDate() > new DateTimeImmutable('now', new DateTimeZone('UTC'))) {
+                    return;
+                }
+
+                $user = $this->security->getUser();
+                assert($user instanceof User);
+
+                $checkoutUrl = $this->subscriptionManager->getCheckoutUrl($subscription, ['email' => $user->getEmail()]);
+                $event->setResponse(
+                    new Response(
+                        $this->twig->render('@SolidInvoiceSaas/subscription/cancelled.html.twig', [
+                            'subscription' => $subscription,
+                            'checkoutUrl' => $checkoutUrl,
+                        ]),
+                    )
+                );
+                break;
+        }
+    }
+
+    public function onResponse(ResponseEvent $event): void
+    {
+        $request = $event->getRequest();
+        $response = $event->getResponse();
+
+        if (! $request->isMethod('GET') || $response->getStatusCode() !== Response::HTTP_OK) {
+            return;
+        }
+
+        $subscription = $this->getSubscription($event->getRequest());
+        if (! $subscription instanceof Subscription) {
+            return;
+        }
+
+        if ($subscription->getStatus() !== SubscriptionStatus::CANCELLED || $subscription->getEndDate() <= new DateTimeImmutable('now', new DateTimeZone('UTC'))) {
+            return;
+        }
+
+        $user = $this->security->getUser();
+        assert($user instanceof User);
+
+        $content = $response->getContent();
+
+        $session = $request->getSession();
+
+        if ($session->has('checkout_url')) {
+            $checkoutUrl = $session->get('checkout_url');
+        } else {
+            $checkoutUrl = $this->subscriptionManager->getCheckoutUrl($subscription, ['email' => $user->getEmail()]);
+            $session->set('checkout_url', $checkoutUrl);
+        }
+
+        $message = '<strong>Subscription Canceled</strong> - Your subscription has been canceled. Your access will be revoked on ' . $subscription->getEndDate()->format('Y-m-d H:i:s') . '.<br /><a href="' . $checkoutUrl . '" class="btn btn-default btn-sm">Re-new now<a/> to avoid losing access.';
+
+        $content = str_replace(
+            '<div class="wrapper">',
+            '<div class="wrapper"><div class="main-header bg-yellow p-2 text-center text-white">' . $message . '</div>',
+            $content,
+        );
+
+        $response->setContent($content);
+    }
+
+    private function getSubscription(Request $request): ?Subscription
+    {
+        if (in_array($request->attributes->get('_route'), self::SKIPPED_ROUTES, true)) {
+            return null;
         }
 
         $companyId = $this->companySelector->getCompany();
 
         if (! $companyId instanceof Ulid) {
-            return;
+            return null;
         }
 
         $company = $this->companyRepository->find($companyId);
 
         if (! $company) {
-            return;
+            return null;
         }
 
         $subscription = $this->subscriptionManager->getSubscriptionFor($company);
-
-        if ($subscription?->getStatus() === SubscriptionStatus::PENDING) {
-            $checkoutUrl = $this->subscriptionManager->getCheckoutUrl($subscription);
-            $event->setResponse(
-                new Response(
-                    $this->twig->render('@SolidInvoiceSaas/subscription/pending.html.twig', [
-                        'subscription' => $subscription,
-                        'checkoutUrl' => $checkoutUrl,
-                    ]),
-                )
-            );
+        if (! $subscription instanceof Subscription) {
+            return null;
         }
+
+        return $subscription;
     }
 }
