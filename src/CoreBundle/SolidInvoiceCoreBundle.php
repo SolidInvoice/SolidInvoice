1108a6d926e811b23022106b6573e552de3d4db9
GitHub Action <action@github.com>
2025-06-20 14:11:27 +0000

    Release version 2.3.8

--- src/CoreBundle/SolidInvoiceCoreBundle.php
+++ src/CoreBundle/SolidInvoiceCoreBundle.php
@@ -19,7 +19,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 final class SolidInvoiceCoreBundle extends Bundle
 {
-    final public const VERSION = '2.3.7';
+    final public const VERSION = '2.3.8';
 
     final public const APP_NAME = 'SolidInvoice';
 
