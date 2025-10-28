5bae85467b3caf496915cd776be818620afa0ffc
Pierre du Plessis <pierre@pcservice.co.za>
2025-10-28 22:38:19 +0200

    Add option to hide Powered By text

---
+++ src/SaasBundle/Config/ConfigProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * This file is part of SolidInvoice project.
+ *
+ * (c) Pierre du Plessis <open-source@solidworx.co>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace SolidInvoice\SaasBundle\Config;
+
+use SolidInvoice\SettingsBundle\Config\ProviderInterface;
+use SolidInvoice\SettingsBundle\DTO\Config;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+
+final class ConfigProvider implements ProviderInterface
+{
+    public function provide(array $data): array
+    {
+        return [
+            new Config('system/general/hide_powered_by', '0', 'Hide "Powered by SolidInvoice" text in invoices and quotes.', CheckboxType::class),
+        ];
+    }
+}
