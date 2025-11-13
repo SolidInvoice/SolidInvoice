1bfaa6a103fb4b0e1a23786016d20c7b9e2c18e8
Pierre du Plessis <pierre@pcservice.co.za>
2025-11-13 14:33:56 +0200

    Add crowdin configuration

--- config/packages/translation.php
+++ config/packages/translation.php
@@ -18,5 +18,43 @@ return static function (FrameworkConfig $config): void {
     $config
         ->defaultLocale(env('SOLIDINVOICE_LOCALE'))
         ->translator()
-        ->fallbacks(['en']);
+        ->fallbacks(['en'])
+    ;
+
+    $config->translator()
+        ->provider('crowdin')
+        ->dsn(env('CROWDIN_DSN')->default(''))
+        ->locales([
+            'en', // English
+            'af', // Afrikaans
+            'ar', // Arabic
+            'ca', // Catalan
+            'zh-CN', // Chinese Simplified
+            'zh-TW', // Chinese Traditional
+            'cs', // Czech
+            'da', // Danish
+            'nl', // Dutch
+            'fi', // Finnish
+            'fr', // French
+            'de', // German
+            'el', // Greek
+            'he', // Hebrew
+            'hu', // Hungarian
+            'it', // Italian
+            'ja', // Japanese
+            'ko', // Korean
+            'no', // Norwegian
+            'pl', // Polish
+            'pt-PT', // Portuguese
+            'pt-BR', // Portuguese, Brazilian
+            'ro', // Romanian
+            'ru', // Russian
+            'sr', // Serbian (Cyrillic)
+            'es-ES', // Spanish
+            'sv-SE', // Swedish
+            'tr', // Turkish
+            'uk', // Ukrainian
+            'vi', // Vietnamese
+        ])
+    ;
 };
