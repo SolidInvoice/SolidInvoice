37d0f6c7f48fddcbd853a1fbe91cc2c2aa0dbac2
Pierre du Plessis <pierre@pcservice.co.za>
2025-11-16 19:20:09 +0200

    Extract translations to translation files

--- config/packages/translation.php
+++ config/packages/translation.php
@@ -11,8 +11,10 @@ declare(strict_types=1);
  * with this source code in the file LICENSE.
  */
 
+use Symfony\Component\Yaml\Yaml;
 use Symfony\Config\FrameworkConfig;
 use function Symfony\Component\DependencyInjection\Loader\Configurator\env;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\param;
 
 return static function (FrameworkConfig $config): void {
     $config
@@ -24,37 +26,6 @@ return static function (FrameworkConfig $config): void {
     $config->translator()
         ->provider('crowdin')
         ->dsn(env('CROWDIN_DSN')->default(''))
-        ->locales([
-            'en', // English
-            'af', // Afrikaans
-            'ar', // Arabic
-            'ca', // Catalan
-            'zh-CN', // Chinese Simplified
-            'zh-TW', // Chinese Traditional
-            'cs', // Czech
-            'da', // Danish
-            'nl', // Dutch
-            'fi', // Finnish
-            'fr', // French
-            'de', // German
-            'el', // Greek
-            'he', // Hebrew
-            'hu', // Hungarian
-            'it', // Italian
-            'ja', // Japanese
-            'ko', // Korean
-            'no', // Norwegian
-            'pl', // Polish
-            'pt-PT', // Portuguese
-            'pt-BR', // Portuguese, Brazilian
-            'ro', // Romanian
-            'ru', // Russian
-            'sr', // Serbian (Cyrillic)
-            'es-ES', // Spanish
-            'sv-SE', // Swedish
-            'tr', // Turkish
-            'uk', // Ukrainian
-            'vi', // Vietnamese
-        ])
+        ->locales(Yaml::parseFile(dirname(__DIR__) . '/locales.yaml'))
     ;
 };
