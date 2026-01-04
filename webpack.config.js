40943172119c3890cc9388a60fec3c8cefc9fb16
Pierre du Plessis <pierre@pcservice.co.za>
2026-01-04 15:06:21 +0200

    Re-design invoice and quote PDF

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ import { codecovWebpackPlugin } from '@codecov/webpack-plugin';
 export default Encore
     .addStyleEntry('login', './assets/scss/login.scss')
     .addStyleEntry('installation', './assets/scss/installation.scss')
+    .addStyleEntry('pdf', './assets/scss/pdf.scss')
     .addEntry('app', './assets/app.ts')
     .enableStimulusBridge('./assets/controllers.json')
     .addPlugin(codecovWebpackPlugin({
