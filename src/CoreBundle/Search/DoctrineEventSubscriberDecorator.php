f0d13054a07d49463ebc54fe52c3daa1d5a4a286
Pierre du Plessis <pierre@pcservice.co.za>
2026-03-08 18:30:15 +0200

    Add DoctrineEventSubscriberDecorator

---
+++ src/CoreBundle/Search/DoctrineEventSubscriberDecorator.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of SolidInvoice project.
+ *
+ * (c) Pierre du Plessis <open-source@solidworx.co>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace SolidInvoice\CoreBundle\Search;
+
+use Doctrine\Persistence\Event\LifecycleEventArgs;
+use Meilisearch\Bundle\EventListener\DoctrineEventSubscriber;
+use SolidWorx\Toggler\ToggleInterface;
+
+final class DoctrineEventSubscriberDecorator
+{
+    public function __construct(
+        private readonly DoctrineEventSubscriber $inner,
+        private readonly ToggleInterface $toggle,
+    ) {
+    }
+
+    public function postUpdate(LifecycleEventArgs $args): void
+    {
+        if ($this->toggle->isActive('meilisearch_search')) {
+            $this->inner->postUpdate($args);
+        }
+    }
+
+    public function postPersist(LifecycleEventArgs $args): void
+    {
+        if ($this->toggle->isActive('meilisearch_search')) {
+            $this->inner->postPersist($args);
+        }
+    }
+
+    public function preRemove(LifecycleEventArgs $args): void
+    {
+        if ($this->toggle->isActive('meilisearch_search')) {
+            $this->inner->preRemove($args);
+        }
+    }
+}
