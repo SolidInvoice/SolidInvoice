41ed9acd3724925d28d9b61bdacccfeaa2915bfa
Pierre du Plessis <pierre@pcservice.co.za>
2026-03-08 10:52:17 +0200

    Fix env vars

--- config/services.php
+++ config/services.php
@@ -38,8 +38,8 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters->set('env(SOLIDINVOICE_MESSENGER_DSN)', 'doctrine://default?queue_name=async');
     $parameters->set('env(SOLIDINVOICE_PLATFORM)', null);
 
-    $parameters->set('env(SOLIDINVOICE_MEILISEARCH_URL)', null);
-    $parameters->set('env(SOLIDINVOICE_MEILISEARCH_API_KEY)', null);
+    $parameters->set('env(SOLIDINVOICE_MEILISEARCH_URL)', '');
+    $parameters->set('env(SOLIDINVOICE_MEILISEARCH_API_KEY)', '');
     $parameters->set('env(SOLIDINVOICE_MEILISEARCH_PREFIX)', 'solidinvoice_%env(SOLIDINVOICE_ENV)%_');
 
     if ($containerConfigurator->env() === 'test') {
