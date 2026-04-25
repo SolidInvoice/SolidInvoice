8f5dc51333a4a98c0b55b63d0ee604cc73e7f5ff
Pierre du Plessis <pierre@pcservice.co.za>
2026-04-25 07:06:08 +0200

    feat(core): add CustomFieldTarget enum

---
+++ src/CoreBundle/Enum/CustomFieldTarget.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of SolidInvoice project.
+ *
+ * (c) Pierre du Plessis <open-source@solidworx.co>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace SolidInvoice\CoreBundle\Enum;
+
+enum CustomFieldTarget: string
+{
+    case CLIENT = 'CLIENT';
+    case CONTACT = 'CONTACT';
+
+    public function label(): string
+    {
+        return match ($this) {
+            self::CLIENT => 'Client',
+            self::CONTACT => 'Contact',
+        };
+    }
+}
