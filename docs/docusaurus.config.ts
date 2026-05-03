892e6a9ebeb171acaf8a3f8e66631bbb44569abb
Pierre du Plessis <pierre@pcservice.co.za>
2026-05-03 19:14:40 +0200

    Update docs base url and path

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -11,8 +11,8 @@ const config: Config = {
     v4: true,
   },
 
-  url: 'https://docs.solidinvoice.co',
-  baseUrl: '/',
+  url: 'https://solidinvoice.co',
+  baseUrl: '/docs/',
 
   organizationName: 'SolidInvoice',
   projectName: 'SolidInvoice',
