95ed28eb6906a48a3b23b0d8b68d491da93a1361
Pierre du Plessis <pierre@pcservice.co.za>
2026-05-04 12:33:40 +0200

    Fix types in docs

--- docs/src/theme/CodeBlock/Buttons/CopyButton/index.tsx
+++ docs/src/theme/CodeBlock/Buttons/CopyButton/index.tsx
@@ -2,19 +2,23 @@ import React, {type ReactNode, useCallback} from 'react';
 import CopyButton from '@theme-original/CodeBlock/Buttons/CopyButton';
 import type CopyButtonType from '@theme/CodeBlock/Buttons/CopyButton';
 import type {WrapperProps} from '@docusaurus/types';
+import {useCodeBlockContext} from '@docusaurus/theme-common/internal';
 import {track} from '@site/src/lib/analytics';
 
 type Props = WrapperProps<typeof CopyButtonType>;
 
 export default function CopyButtonWrapper(props: Props): ReactNode {
+  const {metadata} = useCodeBlockContext();
+  const language = metadata.language;
+
   const handleClick = useCallback(() => {
     if (typeof window === 'undefined') return;
     track({
       event: 'docs_copy_code',
-      language: props.language ?? 'unknown',
+      language: language ?? 'unknown',
       source_page: window.location.pathname,
     });
-  }, [props.language]);
+  }, [language]);
 
   return (
     <span onClickCapture={handleClick}>
