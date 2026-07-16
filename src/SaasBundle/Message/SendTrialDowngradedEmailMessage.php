0bddeb699eb75e3790522c4d9f495fc1290d9134
Pierre du Plessis <pierre@pcservice.co.za>
2026-07-16 15:49:34 +0200

    feat(saas): add trial-downgraded email content

---
+++ src/SaasBundle/Message/SendTrialDowngradedEmailMessage.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of SolidInvoice project.
+ *
+ * (c) Pierre du Plessis <open-source@solidworx.co>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace SolidInvoice\SaasBundle\Message;
+
+use Symfony\Component\Uid\Ulid;
+
+final readonly class SendTrialDowngradedEmailMessage
+{
+    public function __construct(
+        public Ulid $subscriptionId,
+    ) {
+    }
+}
