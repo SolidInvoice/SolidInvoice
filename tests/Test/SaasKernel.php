1eaf932fa0b4c3e6ace5bc4ca20f25883df50f3a
Pierre du Plessis <pierre@pcservice.co.za>
2026-07-31 12:41:26 +0200

    Add missing kernel classes

---
+++ tests/Test/SaasKernel.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of SolidInvoice project.
+ *
+ * (c) Pierre du Plessis <open-source@solidworx.co>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace SolidInvoice\Test;
+
+use SolidInvoice\AppMode;
+
+class SaasKernel extends \SolidInvoice\Kernel
+{
+    public function __construct(string $environment, bool $debug)
+    {
+        parent::__construct(AppMode::SAAS, $environment, $debug);
+    }
+
+    public function getCacheDir(): string
+    {
+        return $this->getProjectDir() . '/var/cache/' . $this->environment . '/saas';
+    }
+}
