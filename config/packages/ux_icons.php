7246a935c6f504345b0a9d4f1ccec7d6821c84f3
Pierre du Plessis <pierre@pcservice.co.za>
2026-09-04 21:12:35 +0200

    Update icons, modal and snapshots

---
+++ config/packages/ux_icons.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of SolidInvoice project.
+ *
+ * (c) Pierre du Plessis <open-source@solidworx.co>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Symfony\Component\DependencyInjection\Loader\Configurator;
+
+return App::config([
+    'ux_icons' => [
+        'iconify' => [
+            'enabled' => false,
+        ],
+    ],
+]);
